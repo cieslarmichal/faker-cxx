eea20b38c3b2159f78456f598ce62e6a772247ac
Michal Cieslar <michalovskyyy@gmail.com>
2023-06-26 15:29:10 +0200

    add last names data

---
+++ src/modules/person/data/LastNames.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+const std::vector<std::string> lastNames = {
+    "Johnson",   "Smith",    "Brown",     "Williams", "Garcia",    "Jones",    "Davis",      "Miller",  "Martinez",
+    "Rodriguez", "Lopez",    "Hernandez", "Wilson",   "Gonzales",  "Thomas",   "Anderson",   "Moore",   "Taylor",
+    "Martin",    "Jackson",  "Perez",     "Lee",      "White",     "Thompson", "Sanchez",    "Harris",  "Ramirez",
+    "Clark",     "Robinson", "Lewis",     "Young",    "Walker",    "King",     "Allen",      "Scott",   "Wright",
+    "Nguyen",    "Torres",   "Flores",    "Hill",     "Adams",     "Green",    "Baker",      "Nelson",  "Rivera",
+    "Hall",      "Mitchell", "Campbell",  "Roberts",  "Carter",    "Phillips", "Gomez",      "Turner",  "Evans",
+    "Parker",    "Diaz",     "Edwards",   "Cruz",     "Reyes",     "Collins",  "Morris",     "Stewart", "Murphy",
+    "Morales",   "Rogers",   "Cook",      "Ortiz",    "Gutierrez", "Cooper",   "Morgan",     "Bailey",  "Peterson",
+    "Kelly",     "Reed",     "Ramos",     "Howard",   "Cox",       "Kim",      "Richardson", "Ward",    "Brooks",
+    "Watson",    "Wood",     "Chavez",    "Bennet",   "James",     "Mendoza",  "Gray",       "Hughes",  "Ruiz",
+    "Alvarez",   "Price",    "Sanders",   "Castillo", "Myers",     "Patel",    "Ross",       "Long",    "Jimenez",
+    "Foster",
+};
