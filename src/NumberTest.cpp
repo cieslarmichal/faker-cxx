4c9e7f81e955d0d42c4a49a9013922d62837204b
Michal Cieslar <michalovskyyy@gmail.com>
2023-07-03 19:01:05 +0200

    fix number unit tests

--- src/NumberTest.cpp
+++ src/NumberTest.cpp
@@ -12,7 +12,7 @@ public:
 
 TEST_F(NumberDataGeneratorTest, givenInvalidRangeArguments_shouldThrowInvalidArgument)
 {
-    ASSERT_THROW(Number::integer(10, 2), std::invalid_argument);
+    ASSERT_THROW(Number::integer(2, 10), std::invalid_argument);
 }
 
 TEST_F(NumberDataGeneratorTest, givenRangeWithSameNumberSection_shouldGenerateThisNumber)
@@ -24,7 +24,7 @@ TEST_F(NumberDataGeneratorTest, givenRangeWithSameNumberSection_shouldGenerateTh
 
 TEST_F(NumberDataGeneratorTest, givenValidRangeArguments_shouldGenerateNumberThatIsInGivenRange)
 {
-    const auto actualRandomNumber = Number::integer(2, 10);
+    const auto actualRandomNumber = Number::integer(10, 2);
 
     ASSERT_TRUE(actualRandomNumber >= 2);
     ASSERT_TRUE(actualRandomNumber <= 10);
