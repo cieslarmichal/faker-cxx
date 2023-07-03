4d0f89471179b04d972f5b1c0895dc464a33cd07
Michal Cieslar <michalovskyyy@gmail.com>
2023-07-03 21:14:39 +0200

    add person unit tests

---
+++ src/PersonTest.cpp
@@ -0,0 +1,37 @@
+#include "Person.h"
+
+#include "gtest/gtest.h"
+
+#include "data/FirstNamesFemales.h"
+#include "data/FirstNamesMales.h"
+#include "data/LastNames.h"
+
+using namespace ::testing;
+using namespace faker;
+
+class PersonTest : public Test
+{
+public:
+};
+
+TEST_F(PersonTest, shouldGenerateFirstName)
+{
+    std::vector<std::string> firstNames{firstNamesMales};
+
+    firstNames.insert(firstNames.end(), firstNamesFemales.begin(), firstNamesFemales.end());
+
+    const auto generatedFirstName = Person::firstName();
+
+    ASSERT_TRUE(std::any_of(firstNames.begin(), firstNames.end(),
+                            [generatedFirstName](const std::string& firstName)
+                            { return firstName == generatedFirstName; }));
+}
+
+TEST_F(PersonTest, shouldGenerateLastName)
+{
+    const auto generatedLastName = Person::lastName();
+
+    ASSERT_TRUE(std::any_of(lastNames.begin(), lastNames.end(),
+                            [generatedLastName](const std::string& lastName)
+                            { return lastName == generatedLastName; }));
+}
