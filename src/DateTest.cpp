f7c05adeec9e5e3646cd4c70622f9f46b77c2d6c
Michal Cieslar <michalovskyyy@gmail.com>
2023-07-06 01:13:03 +0200

    add birthdate tests

--- src/DateTest.cpp
+++ src/DateTest.cpp
@@ -11,6 +11,8 @@ namespace
 {
 const auto secondsInYear = 31556926;
 const auto secondsInDay = 86400;
+const auto numberOfHoursInDay = 24;
+const auto numberOfDaysInYear = 365;
 }
 
 class DateTest : public Test
@@ -22,9 +24,11 @@ TEST_F(DateTest, shouldGeneratePastDate)
 {
     const auto currentDate = boost::posix_time::second_clock::local_time();
 
-    const auto pastDate = Date::past();
+    const auto pastYears = 5;
 
-    EXPECT_TRUE((currentDate - pastDate).total_seconds() < secondsInYear);
+    const auto pastDate = Date::past(pastYears);
+
+    EXPECT_TRUE((currentDate - pastDate).total_seconds() < secondsInYear * pastYears);
     EXPECT_TRUE(pastDate < currentDate);
 }
 
@@ -70,9 +74,11 @@ TEST_F(DateTest, shouldGenerateFutureDate)
 {
     const auto currentDate = boost::posix_time::second_clock::local_time();
 
-    const auto futureDate = Date::future();
+    const auto futureYears = 3;
 
-    EXPECT_TRUE((futureDate - currentDate).total_seconds() < secondsInYear);
+    const auto futureDate = Date::future(futureYears);
+
+    EXPECT_TRUE((futureDate - currentDate).total_seconds() < secondsInYear * futureYears);
     EXPECT_TRUE(futureDate > currentDate);
 }
 
@@ -96,7 +102,7 @@ TEST_F(DateTest, shouldGenerateSoonDate)
 
     const auto soonDate = Date::soon(soonDays);
 
-    EXPECT_TRUE((soonDate - currentDate).total_seconds() <  secondsInDay * soonDays);
+    EXPECT_TRUE((soonDate - currentDate).total_seconds() < secondsInDay * soonDays);
     EXPECT_TRUE(soonDate > currentDate);
 }
 
@@ -110,6 +116,66 @@ TEST_F(DateTest, shouldGenerateSoonDateISO)
 
     const auto soonDate = boost::posix_time::from_iso_extended_string(soonDateISO);
 
-    EXPECT_TRUE((soonDate - currentDate).total_seconds() <  secondsInDay * soonDays);
+    EXPECT_TRUE((soonDate - currentDate).total_seconds() < secondsInDay * soonDays);
     EXPECT_TRUE(soonDate > currentDate);
+}
+
+TEST_F(DateTest, shouldGenerateDateFromRange)
+{
+    const auto startDate =
+        boost::posix_time::second_clock::local_time() - boost::posix_time::hours(numberOfHoursInDay * 2);
+    const auto endDate =
+        boost::posix_time::second_clock::local_time() + boost::posix_time::hours(numberOfHoursInDay * 2);
+
+    const auto dateWithinRage = Date::fromRange(startDate, endDate);
+
+    EXPECT_TRUE(dateWithinRage > startDate);
+    EXPECT_TRUE(dateWithinRage < endDate);
+}
+
+TEST_F(DateTest, shouldGenerateBirthDateByAge)
+{
+    const auto birthDate = Date::birthDateByAge(25, 30);
+
+    const auto expectedStartDate = boost::posix_time::second_clock::local_time() -
+                                   boost::posix_time::hours(numberOfHoursInDay * numberOfDaysInYear * 30);
+
+    const auto expectedEndDate = boost::posix_time::second_clock::local_time() -
+                                 boost::posix_time::hours(numberOfHoursInDay * numberOfDaysInYear * 25);
+
+    EXPECT_TRUE(birthDate > expectedStartDate);
+    EXPECT_TRUE(birthDate < expectedEndDate);
+}
+
+TEST_F(DateTest, shouldGenerateBirthDateByAgeISO)
+{
+    const auto birthDateISO = Date::birthDateByAgeISOString(5, 15);
+
+    const auto birthDate = boost::posix_time::from_iso_extended_string(birthDateISO);
+
+    const auto expectedStartDate = boost::posix_time::second_clock::local_time() -
+                                   boost::posix_time::hours(numberOfHoursInDay * numberOfDaysInYear * 15);
+
+    const auto expectedEndDate = boost::posix_time::second_clock::local_time() -
+                                 boost::posix_time::hours(numberOfHoursInDay * numberOfDaysInYear * 5);
+
+    EXPECT_TRUE(birthDate > expectedStartDate);
+    EXPECT_TRUE(birthDate < expectedEndDate);
+}
+
+TEST_F(DateTest, shouldGenerateBirthDateByYear)
+{
+    const auto birthDate = Date::birthDateByYear(2000, 2023);
+
+    EXPECT_TRUE(birthDate.date().year() >= 2000);
+    EXPECT_TRUE(birthDate.date().year() <= 2023);
+}
+
+TEST_F(DateTest, shouldGenerateBirthDateByYearISO)
+{
+    const auto birthDateISO = Date::birthDateByYearISOString(1996, 1996);
+
+    const auto birthDate = boost::posix_time::from_iso_extended_string(birthDateISO);
+
+    EXPECT_TRUE(birthDate.date().year() == 1996);
 }
