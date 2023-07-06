767d38031d0f3af9fe7574e8d6dcc6577db11a7f
Michal Cieslar <michalovskyyy@gmail.com>
2023-07-06 18:10:06 +0200

    add internet docs

--- include/faker-cxx/Internet.h
+++ include/faker-cxx/Internet.h
@@ -8,11 +8,55 @@ namespace faker
 class Internet
 {
 public:
+    /**
+     * @brief Generates a username using the given person's name as base.
+     *
+     * @param firstName The optional first name to include in username.
+     * @param lastName The optional last name to include in username.
+     *
+     * @returns Username including neither, one or both of the provided names
+     *
+     * @example
+     * Internet::username() // "Richardson.Jeffrey1997"
+     * Internet::username("Michael") // "Michael_Allen29"
+     * Internet::username(std::nullopt, "Cieslar") // "Phillip_Cieslar20"
+     * Internet::username("Andrew", "Cieslar") // "Andrew.Cieslar"
+     */
     static std::string username(std::optional<std::string> firstName = std::nullopt,
                                 std::optional<std::string> lastName = std::nullopt);
 
-    static std::string email();
+    /**
+     * @brief Generates an email address using the given person's name as base.
+     *
+     * @param firstName The optional first name to use. If not specified, a random one will be chosen.
+     * @param lastName The optional last name to use. If not specified, a random one will be chosen.
+     * @param emailHost The optional email host name to use. If not specified, a random one will be chosen.
+     *
+     * @returns Email including neither, one or both of the provided names, with random/provided emailHost
+     *
+     * @example
+     * Internet::email() // "Jimenez.Clyde@gmail.com"
+     * Internet::email("Tom") // "TomRichardson79@outlook.com"
+     * Internet::email(std::nullopt, "Howard") // "FreddieHoward@hotmail.com"
+     * Internet::email("Cindy", "Young") // "Young_Cindy@gmail.com"
+     * Internet::email(std::nullopt, std::nullopt, "example.com") // "Wright.Edna1973@example.com"
+     */
+    static std::string email(std::optional<std::string> firstName = std::nullopt,
+                             std::optional<std::string> lastName = std::nullopt,
+                             std::optional<std::string> emailHost = std::nullopt);
 
+    /**
+     * @brief Generates a random password-like string. Do not use this method for generating actual passwords for users.
+     * Since the source of the randomness is not cryptographically secure, neither is this generator
+     *
+     * @param length The length of the password to generate. Defaults to `15`.
+     *
+     * @returns Random password-like string
+     *
+     * @example
+     * Internet::password() // "gXGpe9pKfFcKy9R"
+     * Internet::password(25) // "xv8vDu*wM!Rg0$zd0kH%8p!WY"
+     */
     static std::string password(int length = 15);
 };
 }
--- src/InternetTest.cpp
+++ src/InternetTest.cpp
@@ -36,7 +36,7 @@ TEST_F(InternetTest, shouldGenerateUsername)
 
 TEST_F(InternetTest, shouldGenerateUsernameWithFirstNameProvided)
 {
-    const auto firstName = "Michal";
+    const auto firstName = "Michael";
 
     const auto username = Internet::username(firstName);
 
@@ -64,7 +64,7 @@ TEST_F(InternetTest, shouldGenerateUsernameWithLastNameProvided)
 
 TEST_F(InternetTest, shouldGenerateUsernameWithFullNameProvided)
 {
-    const auto firstName = "Michal";
+    const auto firstName = "Andrew";
 
     const auto lastName = "Cieslar";
 
@@ -102,9 +102,129 @@ TEST_F(InternetTest, shouldGenerateEmail)
                             { return generatedUsername.find(lastName) != std::string::npos; }));
 }
 
+TEST_F(InternetTest, shouldGenerateEmailWithFirstName)
+{
+    const auto firstName = "Tom";
+
+    const auto email = Internet::email(firstName);
+
+    std::vector<std::string> emailParts;
+
+    boost::split(emailParts, email, boost::is_any_of("@"));
+
+    ASSERT_EQ(emailParts.size(), 2);
+
+    const auto generatedUsername = emailParts[0];
+    const auto generatedEmailHost = emailParts[1];
+
+    ASSERT_TRUE(std::any_of(emailHosts.begin(), emailHosts.end(),
+                            [generatedEmailHost](const std::string& emailHost)
+                            { return generatedEmailHost == emailHost; }));
+    ASSERT_TRUE(generatedUsername.find(firstName) != std::string::npos);
+    ASSERT_TRUE(std::any_of(lastNames.begin(), lastNames.end(),
+                            [generatedUsername](const std::string& lastName)
+                            { return generatedUsername.find(lastName) != std::string::npos; }));
+}
+
+TEST_F(InternetTest, shouldGenerateEmailWithLastName)
+{
+    std::vector<std::string> firstNames{firstNamesMales};
+
+    firstNames.insert(firstNames.end(), firstNamesFemales.begin(), firstNamesFemales.end());
+
+    const auto lastName = "Howard";
+
+    const auto email = Internet::email(std::nullopt, lastName);
+
+    std::vector<std::string> emailParts;
+
+    boost::split(emailParts, email, boost::is_any_of("@"));
+
+    ASSERT_EQ(emailParts.size(), 2);
+
+    const auto generatedUsername = emailParts[0];
+    const auto generatedEmailHost = emailParts[1];
+
+    ASSERT_TRUE(std::any_of(emailHosts.begin(), emailHosts.end(),
+                            [generatedEmailHost](const std::string& emailHost)
+                            { return generatedEmailHost == emailHost; }));
+    ASSERT_TRUE(std::any_of(firstNames.begin(), firstNames.end(),
+                            [generatedUsername](const std::string& firstName)
+                            { return generatedUsername.find(firstName) != std::string::npos; }));
+    ASSERT_TRUE(generatedUsername.find(lastName) != std::string::npos);
+}
+
+TEST_F(InternetTest, shouldGenerateEmailWithFullName)
+{
+    std::vector<std::string> firstNames{firstNamesMales};
+
+    firstNames.insert(firstNames.end(), firstNamesFemales.begin(), firstNamesFemales.end());
+
+    const auto firstName = "Cindy";
+
+    const auto lastName = "Young";
+
+    const auto email = Internet::email(firstName, lastName);
+
+    std::vector<std::string> emailParts;
+
+    boost::split(emailParts, email, boost::is_any_of("@"));
+
+    ASSERT_EQ(emailParts.size(), 2);
+
+    const auto generatedUsername = emailParts[0];
+    const auto generatedEmailHost = emailParts[1];
+
+    ASSERT_TRUE(std::any_of(emailHosts.begin(), emailHosts.end(),
+                            [generatedEmailHost](const std::string& emailHost)
+                            { return generatedEmailHost == emailHost; }));
+    ASSERT_TRUE(generatedUsername.find(firstName) != std::string::npos);
+    ASSERT_TRUE(generatedUsername.find(lastName) != std::string::npos);
+}
+
+TEST_F(InternetTest, shouldGenerateEmailWithSpecifiedEmailHost)
+{
+    std::vector<std::string> firstNames{firstNamesMales};
+
+    firstNames.insert(firstNames.end(), firstNamesFemales.begin(), firstNamesFemales.end());
+
+    const auto emailHost = "example.com";
+
+    const auto email = Internet::email(std::nullopt, std::nullopt, emailHost);
+
+    std::vector<std::string> emailParts;
+
+    boost::split(emailParts, email, boost::is_any_of("@"));
+
+    ASSERT_EQ(emailParts.size(), 2);
+
+    const auto generatedUsername = emailParts[0];
+    const auto generatedEmailHost = emailParts[1];
+
+    ASSERT_EQ(generatedEmailHost, emailHost);
+    ASSERT_TRUE(std::any_of(firstNames.begin(), firstNames.end(),
+                            [generatedUsername](const std::string& firstName)
+                            { return generatedUsername.find(firstName) != std::string::npos; }));
+    ASSERT_TRUE(std::any_of(lastNames.begin(), lastNames.end(),
+                            [generatedUsername](const std::string& lastName)
+                            { return generatedUsername.find(lastName) != std::string::npos; }));
+}
+
 TEST_F(InternetTest, shouldGeneratePassword)
 {
-    const auto passwordLength = 18;
+    const std::string passwordCharacters = "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    const auto password = Internet::password();
+
+    ASSERT_EQ(password.size(), 15);
+    ASSERT_TRUE(std::all_of(password.begin(), password.end(),
+                            [passwordCharacters](char passwordCharacter)
+                            { return passwordCharacters.find(passwordCharacter) != std::string::npos; }));
+}
+
+TEST_F(InternetTest, shouldGeneratePasswordWithSpecifiedLength)
+{
+    const auto passwordLength = 25;
 
     const std::string passwordCharacters = "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
