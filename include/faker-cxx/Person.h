d2be042e2221cb30bd353002fe47e3b684907536
Michal Cieslar <michalovskyyy@gmail.com>
2023-07-06 14:13:53 +0200

    add person docs

--- include/faker-cxx/Person.h
+++ include/faker-cxx/Person.h
@@ -14,8 +14,37 @@ enum class Sex
 class Person
 {
 public:
+    /**
+     * @brief Returns a random first name.
+     *
+     * @param sex The optional sex to use.
+     *
+     * @returns First name starting with a capital letter
+     * @example
+     * Person::firstName() // "Michael"
+     * Person::firstName(Sex::Female) // "Emma"
+     * Person::firstName(Sex::Male) // "Arthur"
+     */
     static std::string firstName(std::optional<Sex> = std::nullopt);
+
+    /**
+     * @brief Returns a random last name.
+     *
+     * @returns Last name starting with a capital letter
+     * @example
+     * Person::lastName() // "Peterson"
+     */
     static std::string lastName();
+
+    /**
+     * @brief Returns a random last name.
+     *
+     * @returns Full name starting with first name
+     * @example
+     * Person::fullName() // "Marcia Robinson"
+     * Person::fullName(Sex::Female) // "Jennifer Martin"
+     * Person::firstName(Sex::Male) // "Samuel Walker"
+     */
     static std::string fullName(std::optional<Sex> = std::nullopt);
 };
 }
