515b26be6d2f56975c4cd2e2c137b501ca36ff52
cieslarmichal <cieslarmichal@users.noreply.github.com>
2023-07-12 08:01:03 +0000

    deploy: 5518ec3ebd5c5cc2db4630adf4c110d70bf70124

--- search/functions_c.js
+++ search/functions_c.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['password_0',['password',['../classfaker_1_1Internet.html#accf91e2599865b20e539b62661c96e23',1,'faker::Internet']]],
   ['pastdate_1',['pastDate',['../classfaker_1_1Date.html#a1dc7c608e07fdb276a1b9cc58d5bb33d',1,'faker::Date']]],
-  ['pin_2',['pin',['../classfaker_1_1Finance.html#a15e187cc0587b8f94c7a41e48ae87ba6',1,'faker::Finance']]]
+  ['pin_2',['pin',['../classfaker_1_1Finance.html#a15e187cc0587b8f94c7a41e48ae87ba6',1,'faker::Finance']]],
+  ['prefix_3',['prefix',['../classfaker_1_1Person.html#a028887fb2a41481096c6547ed1ab57f1',1,'faker::Person']]]
 ];
