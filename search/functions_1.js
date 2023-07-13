d7f084d474a068cf45a7c24288f7409eb2f1d2e3
cieslarmichal <cieslarmichal@users.noreply.github.com>
2023-07-13 22:31:36 +0000

    deploy: d0c1f663b39fbcbd4eec5d7da1431d99099dc7c0

--- search/functions_1.js
+++ search/functions_1.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['bic_0',['bic',['../classfaker_1_1Finance.html#a9b198d6518995033df316182b72b6a19',1,'faker::Finance']]],
   ['birthdatebyage_1',['birthdateByAge',['../classfaker_1_1Date.html#a593f50b064c572aef47d4d7861ce5fbe',1,'faker::Date']]],
-  ['birthdatebyyear_2',['birthdateByYear',['../classfaker_1_1Date.html#a54174b314bed0966a0ee316716322805',1,'faker::Date']]]
+  ['birthdatebyyear_2',['birthdateByYear',['../classfaker_1_1Date.html#a54174b314bed0966a0ee316716322805',1,'faker::Date']]],
+  ['boolean_3',['boolean',['../classfaker_1_1Datatype.html#adcc47b46212ca99e0eb8db4b888a678f',1,'faker::Datatype']]]
 ];
