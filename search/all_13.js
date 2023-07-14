a4b57a76867f2953cc77fadf4f205ea7e7e912b6
cieslarmichal <cieslarmichal@users.noreply.github.com>
2023-07-14 21:47:18 +0000

    deploy: a5e28dde6c46fc01184a835bd823846076293663

--- search/all_13.js
+++ search/all_13.js
@@ -3,5 +3,6 @@ var searchData=
   ['weekdayabbreviatedname_0',['weekdayAbbreviatedName',['../classfaker_1_1Date.html#a27acfb21331807d8cfc0b3c9cfe57927',1,'faker::Date']]],
   ['weekdayname_1',['weekdayName',['../classfaker_1_1Date.html#a04494097d938c26becc981c1130af899',1,'faker::Date']]],
   ['word_2',['Word',['../classfaker_1_1Word.html',1,'faker']]],
-  ['words_3',['words',['../classfaker_1_1Word.html#ad2feb12057a72ae5cba55895da1f3d11',1,'faker::Word']]]
+  ['word_3',['word',['../classfaker_1_1Lorem.html#a8a9728179b08efab84505fbb7ea46eed',1,'faker::Lorem']]],
+  ['words_4',['words',['../classfaker_1_1Lorem.html#aca2ee45cd6137dc43ea791497be506bd',1,'faker::Lorem::words()'],['../classfaker_1_1Word.html#ad2feb12057a72ae5cba55895da1f3d11',1,'faker::Word::words()']]]
 ];
--- search/functions_f.js
+++ search/functions_f.js
@@ -1,8 +1,11 @@
 var searchData=
 [
   ['sample_0',['sample',['../classfaker_1_1Word.html#a2c10f438e9949dd8fcb76a187094d886',1,'faker::Word']]],
-  ['sex_1',['sex',['../classfaker_1_1Person.html#a0347ebf92763183d8eef2fce5f78fc44',1,'faker::Person']]],
-  ['shuffle_2',['shuffle',['../classfaker_1_1Helper.html#a5604817b289605971edea4034bd65115',1,'faker::Helper']]],
-  ['sku_3',['sku',['../classfaker_1_1Commerce.html#af427c184726e56858e7730b0255d2f99',1,'faker::Commerce']]],
-  ['soondate_4',['soonDate',['../classfaker_1_1Date.html#a67910b9ec3d3e0b57285f96a8dfbc659',1,'faker::Date']]]
+  ['sentence_1',['sentence',['../classfaker_1_1Lorem.html#ace402bfeedb5be11d011a38c5708daa5',1,'faker::Lorem']]],
+  ['sentences_2',['sentences',['../classfaker_1_1Lorem.html#a642a0a6fe7293f968b01a46b8202224a',1,'faker::Lorem']]],
+  ['sex_3',['sex',['../classfaker_1_1Person.html#a0347ebf92763183d8eef2fce5f78fc44',1,'faker::Person']]],
+  ['shuffle_4',['shuffle',['../classfaker_1_1Helper.html#a5604817b289605971edea4034bd65115',1,'faker::Helper']]],
+  ['sku_5',['sku',['../classfaker_1_1Commerce.html#af427c184726e56858e7730b0255d2f99',1,'faker::Commerce']]],
+  ['slug_6',['slug',['../classfaker_1_1Lorem.html#a05591c4bbec3ec177e51020805414548',1,'faker::Lorem']]],
+  ['soondate_7',['soonDate',['../classfaker_1_1Date.html#a67910b9ec3d3e0b57285f96a8dfbc659',1,'faker::Date']]]
 ];
