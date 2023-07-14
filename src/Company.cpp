2ec49c6b7e46383f5d22e5e6323f23e4ab013791
Michal Cieslar <michalovskyyy@gmail.com>
2023-07-14 13:19:29 +0200

    add generating buzz phrases and catch phrases

--- src/Company.cpp
+++ src/Company.cpp
@@ -2,17 +2,20 @@
 
 #include <format>
 
-#include "data/LastNames.h"
+#include "data/company/BuzzAdjectives.h"
+#include "data/company/BuzzNouns.h"
+#include "data/company/BuzzVerbs.h"
+#include "data/company/CatchPhraseAdjectives.h"
+#include "data/company/CatchPhraseDescriptors.h"
+#include "data/company/CatchPhraseNouns.h"
+#include "data/company/CompanyTypes.h"
+#include "data/company/Industries.h"
+#include "data/company/Suffixes.h"
 #include "Helper.h"
 #include "Person.h"
 
 namespace faker
 {
-namespace
-{
-const std::vector<std::string> companySuffixes{"Inc", "LLC", "Group"};
-}
-
 std::string Company::name()
 {
     std::string companyName;
@@ -35,5 +38,55 @@ std::string Company::name()
     }
 
     return companyName;
+}
+
+std::string Company::type()
+{
+    return Helper::arrayElement<std::string>(companyTypes);
+}
+
+std::string Company::industry()
+{
+    return Helper::arrayElement<std::string>(companyIndustries);
+}
+
+std::string Company::buzzPhrase()
+{
+    return std::format("{} {} {}", buzzVerb(), buzzAdjective(), buzzNoun());
+}
+
+std::string Company::buzzAdjective()
+{
+    return Helper::arrayElement<std::string>(buzzAdjectives);
+}
+
+std::string Company::buzzNoun()
+{
+    return Helper::arrayElement<std::string>(buzzNouns);
+}
+
+std::string Company::buzzVerb()
+{
+    return Helper::arrayElement<std::string>(buzzVerbs);
+}
+
+std::string Company::catchPhrase()
+{
+    return std::format("{} {} {}", catchPhraseAdjective(), catchPhraseDescriptor(), catchPhraseNoun());
+}
+
+std::string Company::catchPhraseAdjective()
+{
+    return Helper::arrayElement<std::string>(catchPhraseAdjectives);
+}
+
+std::string Company::catchPhraseDescriptor()
+{
+    return Helper::arrayElement<std::string>(catchPhraseDescriptors);
+}
+
+std::string Company::catchPhraseNoun()
+{
+    return Helper::arrayElement<std::string>(catchPhraseNouns);
 }
 }
