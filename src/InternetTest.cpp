76b6cc2da228680a859bf3fdb7ab98bfa75f6768
Michał Cieślar <michalovskyyy@gmail.com>
2023-07-17 23:18:39 +0200

    Feature/internet http (#53)
    * add http status codes and method names generation
    
    * add http status codes and method names generation

--- src/InternetTest.cpp
+++ src/InternetTest.cpp
@@ -1,5 +1,7 @@
 #include "faker-cxx/Internet.h"
 
+#include <algorithm>
+
 #include "gtest/gtest.h"
 
 #include "data/EmailHosts.h"
@@ -7,10 +9,22 @@
 #include "data/FirstNamesMales.h"
 #include "data/LastNames.h"
 #include "StringHelper.h"
-#include <algorithm>
 
 using namespace ::testing;
 using namespace faker;
+
+namespace
+{
+const std::vector<std::string> webProtocols{"http", "https"};
+const std::vector<std::string> httpMethodNames{"GET", "POST", "DELETE", "PATCH", "PUT"};
+const std::vector<unsigned> httpStatusInformationalCodes{100, 101, 102, 103};
+const std::vector<unsigned> httpStatusSuccessCodes{200, 201, 202, 203, 204, 205, 206, 207, 208, 226};
+const std::vector<unsigned> httpStatusRedirectionCodes{300, 301, 302, 303, 304, 305, 306, 307, 308};
+const std::vector<unsigned> httpStatusClientErrorCodes{400, 401, 402, 403, 404, 405, 406, 407, 408, 409,
+                                                       410, 411, 412, 413, 414, 415, 416, 417, 418, 421,
+                                                       422, 423, 424, 425, 426, 428, 429, 431, 451};
+const std::vector<unsigned> httpStatusServerErrorCodes{500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511};
+}
 
 class InternetTest : public Test
 {
@@ -245,4 +259,75 @@ TEST_F(InternetTest, shouldGenerateGithubAvatarUrl)
 
     ASSERT_TRUE(githubAvatarUrl.starts_with(expectedGithubAvatarPrefix));
     ASSERT_TRUE(userNumber >= 0 && userNumber <= 100000000);
+}
+
+TEST_F(InternetTest, shouldGenerateProtocol)
+{
+    const auto webProtocol = Internet::protocol();
+
+    ASSERT_TRUE(std::any_of(webProtocols.begin(), webProtocols.end(),
+                            [webProtocol](const std::string& protocol) { return webProtocol == protocol; }));
+}
+
+TEST_F(InternetTest, shouldGenerateHttpMethod)
+{
+    const auto generatedHttpMethod = Internet::httpMethod();
+
+    ASSERT_TRUE(std::any_of(httpMethodNames.begin(), httpMethodNames.end(),
+                            [generatedHttpMethod](const std::string& httpMethod) { return generatedHttpMethod == httpMethod; }));
+}
+
+TEST_F(InternetTest, shouldGenerateHttpStatusCode)
+{
+    const auto generatedHttpStatusCode = Internet::httpStatusCode();
+
+    std::vector<unsigned> statusCodes;
+    statusCodes.insert(statusCodes.end(), httpStatusInformationalCodes.begin(), httpStatusInformationalCodes.end());
+    statusCodes.insert(statusCodes.end(), httpStatusSuccessCodes.begin(), httpStatusSuccessCodes.end());
+    statusCodes.insert(statusCodes.end(), httpStatusRedirectionCodes.begin(), httpStatusRedirectionCodes.end());
+    statusCodes.insert(statusCodes.end(), httpStatusClientErrorCodes.begin(), httpStatusClientErrorCodes.end());
+    statusCodes.insert(statusCodes.end(), httpStatusServerErrorCodes.begin(), httpStatusServerErrorCodes.end());
+
+    ASSERT_TRUE(std::any_of(statusCodes.begin(), statusCodes.end(),
+                            [generatedHttpStatusCode](unsigned statusCode) { return generatedHttpStatusCode == statusCode; }));
+}
+
+TEST_F(InternetTest, shouldGenerateHttpInformationalSuccessCode)
+{
+    const auto generatedHttpStatusCode = Internet::httpStatusCode(HttpResponseType::Informational);
+
+    ASSERT_TRUE(std::any_of(httpStatusInformationalCodes.begin(), httpStatusInformationalCodes.end(),
+                            [generatedHttpStatusCode](unsigned statusCode) { return generatedHttpStatusCode == statusCode; }));
+}
+
+TEST_F(InternetTest, shouldGenerateHttpStatusSuccessCode)
+{
+    const auto generatedHttpStatusCode = Internet::httpStatusCode(HttpResponseType::Success);
+
+    ASSERT_TRUE(std::any_of(httpStatusSuccessCodes.begin(), httpStatusSuccessCodes.end(),
+                            [generatedHttpStatusCode](unsigned statusCode) { return generatedHttpStatusCode == statusCode; }));
+}
+
+TEST_F(InternetTest, shouldGenerateHttpStatusRedirectionCode)
+{
+    const auto generatedHttpStatusCode = Internet::httpStatusCode(HttpResponseType::Redirection);
+
+    ASSERT_TRUE(std::any_of(httpStatusRedirectionCodes.begin(), httpStatusRedirectionCodes.end(),
+                            [generatedHttpStatusCode](unsigned statusCode) { return generatedHttpStatusCode == statusCode; }));
+}
+
+TEST_F(InternetTest, shouldGenerateHttpStatusClientErrorCode)
+{
+    const auto generatedHttpStatusCode = Internet::httpStatusCode(HttpResponseType::ClientError);
+
+    ASSERT_TRUE(std::any_of(httpStatusClientErrorCodes.begin(), httpStatusClientErrorCodes.end(),
+                            [generatedHttpStatusCode](unsigned statusCode) { return generatedHttpStatusCode == statusCode; }));
+}
+
+TEST_F(InternetTest, shouldGenerateHttpStatusServerErrorCode)
+{
+    const auto generatedHttpStatusCode = Internet::httpStatusCode(HttpResponseType::ServerError);
+
+    ASSERT_TRUE(std::any_of(httpStatusServerErrorCodes.begin(), httpStatusServerErrorCodes.end(),
+                            [generatedHttpStatusCode](unsigned statusCode) { return generatedHttpStatusCode == statusCode; }));
 }
