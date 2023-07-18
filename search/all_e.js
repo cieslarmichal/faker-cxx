f4c6a0d032e6ba780c91cc087f3a36803bbcfde2
cieslarmichal <cieslarmichal@users.noreply.github.com>
2023-07-18 09:42:54 +0000

    deploy: e9b79c31ad343d0605b754d222c1140f4fd2301a

--- search/all_e.js
+++ search/all_e.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['recentdate_0',['recentDate',['../classfaker_1_1Date.html#a7c47fce4110d1e4f06642871206a536b',1,'faker::Date']]],
-  ['rgb_1',['rgb',['../classfaker_1_1Color.html#a42bdb9b28f7e1b876fe63bea90f61406',1,'faker::Color']]]
+  ['rgb_1',['rgb',['../classfaker_1_1Color.html#a42bdb9b28f7e1b876fe63bea90f61406',1,'faker::Color']]],
+  ['routingnumber_2',['routingNumber',['../classfaker_1_1Finance.html#a630a739b5fefc9cb3cdf264bc27db2aa',1,'faker::Finance']]]
 ];
