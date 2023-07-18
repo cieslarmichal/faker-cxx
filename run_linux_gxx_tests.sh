5dc324519b9e7ca02c5a0899c26d7fef99a01786
Michal Cieslar <michalovskyyy@gmail.com>
2023-07-19 00:47:03 +0200

    add linux g++ build github workflow

---
+++ run_linux_gxx_tests.sh
@@ -0,0 +1,11 @@
+mkdir build-linux-gxx
+
+cd build-linux-gxx || exit 1
+
+cmake .. -DCMAKE_BUILD_TYPE=Debug \
+         -DCMAKE_CXX_COMPILER=/usr/bin/g++-13 \
+         -GNinja
+
+cmake --build .
+
+ctest -C Debug --output-on-failure
