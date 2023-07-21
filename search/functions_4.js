33016b15804082984fbe0353727e0e91c129b6ac
cieslarmichal <cieslarmichal@users.noreply.github.com>
2023-07-21 14:12:54 +0000

    deploy: 32546a9c73b9c4eb27a98d411dbba1f096405313

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['email_0',['email',['../classfaker_1_1Internet.html#af39e73f7b102e8ae73a9ebee7693ad2c',1,'faker::Internet']]]
+  ['email_0',['email',['../classfaker_1_1Internet.html#af39e73f7b102e8ae73a9ebee7693ad2c',1,'faker::Internet']]],
+  ['exampleemail_1',['exampleEmail',['../classfaker_1_1Internet.html#a97776d509e7284b2663aa5631dc21bbc',1,'faker::Internet']]]
 ];
