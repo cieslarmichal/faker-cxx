fa4142042f90620cb4ca841726761085ec6bac8c
Oleksandr Shekera <92540910+shekeraoleksandr@users.noreply.github.com>
2023-07-25 15:40:42 +0300

    Add boolean method with probability definition

--- src/modules/datatype/DatatypeTest.cpp
+++ src/modules/datatype/DatatypeTest.cpp
@@ -21,3 +21,18 @@ TEST_F(DatatypeTest, shouldGenerateBoolean)
     ASSERT_TRUE(std::any_of(booleanValues.begin(), booleanValues.end(),
                             [boolean](bool booleanValue) { return boolean == booleanValue; }));
 }
+
+TEST_F(DatatypeTest, BooleanWithProbTest)
+{
+    bool result2 = Datatype::boolean(0.3);
+    EXPECT_TRUE(result2 || !result2);
+
+    bool result3 = Datatype::boolean(0.8);
+    EXPECT_TRUE(result3 || !result3);
+
+    bool result4 = Datatype::boolean(0.0);
+    EXPECT_FALSE(result4);
+
+    bool result5 = Datatype::boolean(1.0);
+    EXPECT_TRUE(result5);
+}
