e1f7fb9978a7ee79bc3f58dec8d6d077be519d67
Johannes Wilken <7515971+jsbwilken@users.noreply.github.com>
2023-08-04 22:58:22 +0200

    Added iban support for missing eu members

--- include/faker-cxx/types/IbanCountry.h
+++ include/faker-cxx/types/IbanCountry.h
@@ -6,10 +6,33 @@ namespace faker
 {
 enum class IbanCountry
 {
-    Poland,
+    Austria,
+    Belgium,
+    Bulgaria,
+    Croatia,
+    Cyprus,
+    Czechia,
+    Denmark,
+    Estonia,
+    Finland,
     France,
     Germany,
-    Italy
+    Greece,
+    Hungary,
+    Ireland,
+    Italy,
+    Latvia,
+    Lithuania,
+    Luxembourg,
+    Malta,
+    Netherlands,
+    Poland,
+    Portugal,
+    Romania,
+    Slovakia,
+    Slovenia,
+    Spain,
+    Sweden
 };
 
 const std::vector<IbanCountry> supportedIbanCountries{IbanCountry::Poland, IbanCountry::France, IbanCountry::Germany,
