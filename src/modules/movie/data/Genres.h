65a7634eac11ec476bbfceabaf3e9335504c63b9
Michał Cieślar <michalovskyyy@gmail.com>
2023-08-12 14:17:14 +0200

    add movie module (#78)

---
+++ src/modules/movie/data/Genres.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace faker
+{
+const std::vector<std::string> genres = {
+    "Action", "Comedy", "Drama", "Fantasy", "Horror", "Mystery", "Romance", "Thriller", "Western",
+};
+}
