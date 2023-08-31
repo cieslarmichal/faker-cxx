ee6771f6320bf63cc83b01c43a5b5bc2abe0199e
eric-bodhi <erixefb@gmail.com>
2023-08-30 21:05:59 -0400

    added romanian lang support for person

---
+++ src/modules/person/data/romanian/RomanianFirstNamesFemales.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace faker
+{
+const std::vector<std::string> romanianFirstNamesFemales = {
+    "Maria",    "Ioana",     "Elena",    "Ana",      "Cristina", "Andreea",   "Georgiana", "Alexandra", "Laura",
+    "Diana",    "Raluca",    "Mihaela",  "Larisa",   "Monica",   "Alina",     "Simona",    "Gabriela",  "Roxana",
+    "Denisa",   "Camelia",   "Adriana",  "Nicoleta", "Mariana",  "Carmen",    "Loredana",  "Irina",     "Liliana",
+    "Eva",      "Elisabeta", "Aurelia",  "Cătălina", "Silvia",   "Ana-Maria", "Doina",     "Florina",   "Magdalena",
+    "Stefania", "Valentina", "Iuliana",  "Ruxandra", "Georgeta", "Marilena",  "Cătălina",  "Liliana",   "Lidia",
+    "Anda",     "Marinela",  "Mirela",   "Carmen",   "Flavia",   "Claudia",   "Ioana",     "Iuliana",   "Viorica",
+    "Sorina",   "Ramona",    "Camelia",  "Sorana",   "Floriana", "Raluca",    "Mihaela",   "Livia",     "Teodora",
+    "Magda",    "Georgiana", "Adriana",  "Marina",   "Dana",     "Cristina",  "Elena",     "Loredana",  "Lavinia",
+    "Diana",    "Daniela",   "Gabriela", "Simona",   "Nicoleta", "Roxana",    "Valeria",   "Alexandra", "Monica",
+    "Alina",    "Eugenia",   "Denisa",   "Anca",     "Delia",    "Daria",     "Oana",
+};
+}
---
+++ src/modules/person/data/romanian/RomanianFirstNamesMales.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace faker
+{
+const std::vector<std::string> romanianFirstNamesMales = {
+    "Ion",       "Mihai",    "Andrei",   "Alexandru",  "George",  "Cristian",  "Florin",     "Răzvan",   "Lucian",
+    "Adrian",    "Vlad",     "Gheorghe", "Constantin", "Daniel",  "Bogdan",    "Eduard",     "Valentin", "Gabriel",
+    "Dumitru",   "Radu",     "Ștefan",   "Marin",      "Marian",  "Victor",    "Raul",       "Valeriu",  "Bogdan",
+    "Cosmin",    "Marius",   "Mihail",   "Claudiu",    "Ciprian", "Ionel",     "Cristinel",  "Cătălin",  "Tudor",
+    "Cornel",    "Corneliu", "Romeo",    "Romeu",      "Vasile",  "Petre",     "Alex",       "Cătălin",  "Catalin",
+    "Andrei",    "Liviu",    "Ionuț",    "Nicolae",    "Nicoale", "Octavian",  "Răzvan",     "Radu",     "Răzvan",
+    "Ionuț",     "Iulian",   "Dan",      "Emil",       "Eugen",   "Horia",     "Gheorghiță", "Ghiță",    "Horea",
+    "Mihail",    "Mihai",    "Adrian",   "Marian",     "Matei",   "Nicolae",   "Radu",       "Răzvan",   "Sebastian",
+    "Șerban",    "Ștefan",   "Valentin", "Vasile",     "Victor",  "Viorel",    "Cristian",   "Nicolae",  "Laurențiu",
+    "Laurentiu", "Ionuț",    "Nicolae",  "Ionuț",      "Nicolae", "Alexandru", "Alin",       "Ion",      "Gavril",
+    "Alexandru", "Lucian",   "Mihai",    "Mihai",      "Mihail",  "Mihai",     "Marius",     "Ion",      "Nicolae",
+    "Ovidiu",    "Răzvan",   "Radu",     "Sorin",      "Ștefan",  "Tiberiu",
+};
+}
