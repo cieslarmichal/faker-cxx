9a5155a52b394e183f5d9dda4001444357515c87
Romain CLEMENT <rc2301@orange.fr>
2023-10-04 08:44:14 +0000

    Added Animal methods

---
+++ include/faker-cxx/Animal.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+
+namespace faker
+{
+class Animal
+{
+public:
+    static std::string bear();
+
+    static std::string bird();
+
+    static std::string cat();
+
+    static std::string cetacean();
+
+    static std::string cow();
+
+    static std::string crocodilia();
+
+    static std::string dog();
+
+    static std::string fish();
+    
+    static std::string horse();
+    
+    static std::string insect();
+    
+    static std::string lion();
+    
+    static std::string rabbit();
+    
+    static std::string rodent();
+    
+    static std::string snake();
+
+    static std::string type();
+};
+}
