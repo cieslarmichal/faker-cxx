599556a76a1613dfee680809591476dce7f5f781
sandeshkhadka <sandeshkhadka217@gmail.com>
2023-10-14 21:33:25 +0545

    add Medicine module

---
+++ src/modules/medicine/data/medicaltests.h
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+
+namespace faker
+{
+const std::vector<std::string> medicalTests = {
+    "abdominal palpation",
+    "cardiac auscultation",
+    "HEENT examination",
+    "digital rectal examination",
+    "neurological examination",
+    "psychiatric assessment",
+    "pulmonary auscultation",
+    "vaginal examination",
+    "coronary catheterization",
+    "echocardiography",
+    "electrocardiogram",
+    "ballistocardiogram",
+    "skin biopsy",
+    "hearing test",
+    "laryngoscopy",
+    "capsule endoscopy",
+    "coloscopy",
+    "endoscopic retrograde cholangiopancreatography",
+    "esophagogastroduodenoscopy",
+    "esophageal motility study",
+    "esophageal pH monitoring",
+    "liver biopsy",
+    "electroencephalogram",
+    "electromyography",
+    "neuropsychological tests",
+    "amniocentesis",
+    "colposcopy",
+    "mammography",
+    "hysteroscopy",
+    "laparoscopy",
+    "polysomnography",
+    "pulmonary plethysmography",
+    "thoracentesis",
+    "cystoscopy",
+    "urodynamic testing",
+};
+}
