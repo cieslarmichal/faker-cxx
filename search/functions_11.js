f899e70ae4a648294ea514b0808a0eb05f458d1a
cieslarmichal <cieslarmichal@users.noreply.github.com>
2023-10-16 09:57:01 +0000

    deploy: d85b2301ca1af67a3df17fb5bfc4731236708f68

--- search/functions_11.js
+++ search/functions_11.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['timezone_0',['timeZone',['../classfaker_1_1Location.html#a90d40c1120e2719bb3a04e66a7c59e0c',1,'faker::Location']]],
   ['title_1',['title',['../classfaker_1_1Book.html#a74078db73d26c2b148c16992d4045348',1,'faker::Book']]],
-  ['type_2',['type',['../classfaker_1_1Animal.html#ac3a7143378d697aee5a7fb31937ee2dc',1,'faker::Animal::type()'],['../classfaker_1_1Company.html#aaf9b201d720d7bc14cedb88285356ef0',1,'faker::Company::type()']]]
+  ['tvshow_2',['tvShow',['../classfaker_1_1Movie.html#a6380a88c2ae96adf72167d24c46d4055',1,'faker::Movie']]],
+  ['type_3',['type',['../classfaker_1_1Animal.html#ac3a7143378d697aee5a7fb31937ee2dc',1,'faker::Animal::type()'],['../classfaker_1_1Company.html#aaf9b201d720d7bc14cedb88285356ef0',1,'faker::Company::type()']]]
 ];
