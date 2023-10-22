2d1bcef2568289969eb78f946574642ccea689a8
cieslarmichal <cieslarmichal@users.noreply.github.com>
2023-10-22 05:10:53 +0000

    deploy: 63ecaa15aa09372cd4b80cfc050bc8b4117d0326

--- search/functions_c.js
+++ search/functions_c.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['name_0',['name',['../classfaker_1_1Color.html#a75edc9ff2a0cef9b02c998d123d18821',1,'faker::Color::name()'],['../classfaker_1_1Company.html#a2bdc843b50fd6a12217706ccc1f265ea',1,'faker::Company::name()']]],
-  ['networkinterface_1',['networkInterface',['../classfaker_1_1System.html#a04c56b9fa4fd0279417b1e0f3e6174fc',1,'faker::System']]],
-  ['noun_2',['noun',['../classfaker_1_1Hacker.html#a4891fc46bc1412cee6eeb5bf4d4f83a3',1,'faker::Hacker::noun()'],['../classfaker_1_1Word.html#a6f644dfb44d19e572d5e94385c7b9a72',1,'faker::Word::noun()']]],
-  ['number_3',['number',['../classfaker_1_1Phone.html#a59cd1fd7afba171d88413d3a7205ae14',1,'faker::Phone::number(std::optional&lt; std::string &gt;=std::nullopt)'],['../classfaker_1_1Phone.html#a3c4add3af70eb644ded6f99006e27298',1,'faker::Phone::number(PhoneNumberCountryFormat format)']]],
-  ['numeric_4',['numeric',['../classfaker_1_1String.html#ae89b77056f5e13e5bf0418d6a6c231fd',1,'faker::String']]]
+  ['nationality_1',['nationality',['../classfaker_1_1Person.html#a13c40a4e8dfb2738288ea0c1d01c2076',1,'faker::Person']]],
+  ['networkinterface_2',['networkInterface',['../classfaker_1_1System.html#a04c56b9fa4fd0279417b1e0f3e6174fc',1,'faker::System']]],
+  ['noun_3',['noun',['../classfaker_1_1Hacker.html#a4891fc46bc1412cee6eeb5bf4d4f83a3',1,'faker::Hacker::noun()'],['../classfaker_1_1Word.html#a6f644dfb44d19e572d5e94385c7b9a72',1,'faker::Word::noun()']]],
+  ['number_4',['number',['../classfaker_1_1Phone.html#a59cd1fd7afba171d88413d3a7205ae14',1,'faker::Phone::number(std::optional&lt; std::string &gt;=std::nullopt)'],['../classfaker_1_1Phone.html#a3c4add3af70eb644ded6f99006e27298',1,'faker::Phone::number(PhoneNumberCountryFormat format)']]],
+  ['numeric_5',['numeric',['../classfaker_1_1String.html#ae89b77056f5e13e5bf0418d6a6c231fd',1,'faker::String']]]
 ];
