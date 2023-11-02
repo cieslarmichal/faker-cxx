aaebe6533d1d9bd40ead5288947a6d974518e990
cieslarmichal <cieslarmichal@users.noreply.github.com>
2023-11-02 17:46:08 +0000

    deploy: f1527953fd64f97ead0827875fa2dfbd2053ad44

--- search/functions_a.js
+++ search/functions_a.js
@@ -3,7 +3,8 @@ var searchData=
   ['language_0',['language',['../classfaker_1_1Person.html#af414b9dd8565995f4afc0e2850dde42c',1,'faker::Person']]],
   ['lastname_1',['lastName',['../classfaker_1_1Person.html#a364e30d0ee90e2f3359dc7258b798521',1,'faker::Person']]],
   ['latitude_2',['latitude',['../classfaker_1_1Location.html#afd485d36177f0a73c908a48f084c9af4',1,'faker::Location']]],
-  ['lion_3',['lion',['../classfaker_1_1Animal.html#aa7ac1915e26c8e3bf02f1c3ab7601c9c',1,'faker::Animal']]],
-  ['litecoinaddress_4',['litecoinAddress',['../classfaker_1_1Finance.html#a4080af385e6dfb6f82009eac1cb4d956',1,'faker::Finance']]],
-  ['longitude_5',['longitude',['../classfaker_1_1Location.html#afccdf97e9258dbaf29cf4c41aaec8f4e',1,'faker::Location']]]
+  ['lch_3',['lch',['../classfaker_1_1Color.html#aeeff85d92a85f03f27960c6e1bd73c14',1,'faker::Color']]],
+  ['lion_4',['lion',['../classfaker_1_1Animal.html#aa7ac1915e26c8e3bf02f1c3ab7601c9c',1,'faker::Animal']]],
+  ['litecoinaddress_5',['litecoinAddress',['../classfaker_1_1Finance.html#a4080af385e6dfb6f82009eac1cb4d956',1,'faker::Finance']]],
+  ['longitude_6',['longitude',['../classfaker_1_1Location.html#afccdf97e9258dbaf29cf4c41aaec8f4e',1,'faker::Location']]]
 ];
