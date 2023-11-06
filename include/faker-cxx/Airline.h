32a11d520d5819350dcb566aa9aad96dcaddc6b6
Eric Floyd <erixefb@gmail.com>
2023-11-06 13:27:09 -0500

    Created Airline headers and source filse, compiles

---
+++ include/faker-cxx/Airline.h
@@ -0,0 +1,140 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace faker {
+class Airline {
+public:
+    enum class AircraftType
+    {
+        Regional,
+        Narrowbody,
+        Widebody,
+    };
+
+    /*
+     * @brief Get a random aircraft type
+     *
+     * @return a random aircraft type
+     *
+     * @code
+     * Airline::aircraftType // "narrowbody"
+     */
+    static std::string aircraftType();
+
+    struct Airplane
+    {
+        std::string name;
+        std::string iataTypeCode;
+    };
+
+    /*
+     * @brief Get a random airplane
+     *
+     * @return a random airplane and its iataTypeCode
+     *
+     * @code
+     * Airline::airplane() // {"Boeing 737-800", "738"}
+     * @endcode
+     */
+    static Airplane airplane();
+
+    struct AirlineStruct
+    {
+        std::string name;
+        std::string iataCode;
+    };
+
+    /*
+     * @brief Get a random airline
+     *
+     * @return a random airline and its iataCode
+     *
+     * @code
+     * Airline::airline() // {"Air Canada", "AC"}
+     * @endcode
+     */
+    static AirlineStruct airline();
+
+    struct Airport
+    {
+        std::string name;
+        std::string iataCode;
+    };
+
+    /*
+     * @brief Get a random airport
+     *
+     * @return a random airport and its iataCode
+     *
+     * @code
+     * Airline::airport() // {"Toronto Pearson International Airport", "YYZ"}
+     * @endcode
+     */
+    static Airport airport();
+
+    /*
+     * @brief Get a random seat by aircraft type
+     *
+     * @param aircraftType the aircraft type
+     *
+     * @return a random seat
+     *
+     * @code
+     * Airline::seat(AircraftType::Narrowbody) // "1A"
+     * @endcode
+     */
+    static std::string seat(AircraftType aircraftType);
+
+    /*
+     * @brief Get a random record location
+     *
+     * @return a random record location
+     *
+     * @code
+     * Airline::recordLocator() // "ABCDEF"
+     * Airline::recordLocator(true) // "ABC123"
+     * @endcode
+     */
+    static std::string recordLocator(bool allowNumerics = false);
+
+    /*
+     * @brief Get a random flight number from given length
+     *
+     * @param addLeadingZeros whether to add leading zeros
+     *
+     * @param length the length of the flight number
+     *
+     * @return a random flight number
+     *
+     * @code
+     * Airline::flightNumber() // "1234"
+     * Airline::flightNumber(true) // "0123"
+     * Airline::flightNumber(false, 3) // "234"
+     * @endcode
+     */
+    static std::string flightNumber(bool addLeadingZeros = false, unsigned int length = 4);
+
+    struct Range {
+        unsigned int min;
+        unsigned int max;
+    };
+    /*
+     * @brief Get a random flight number from given length
+     *
+     * @param addLeadingZeros whether to add leading zeros
+     *
+     * @param length the length of the flight number
+     *
+     * @return a random flight number
+     *
+     * @code
+     * Airline::flightNumber() // "1234"
+     * Airline::flightNumber(true) // "0123"
+     * Airline::flightNumber(false, {1, 4}) // "234" // "12" // "1234"
+     * @endcode
+     */
+    static std::string flightNumber(bool addLeadingZeros = false, Range length = {1, 4});
+};
+}
---
+++ src/modules/airline/data/Seat.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "faker-cxx/Airline.h"
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+namespace faker
+{
+
+const std::unordered_map<Airline::AircraftType, int> aircraftTypeMaxRows =
+    {
+        {Airline::AircraftType::Regional, 20},
+        {Airline::AircraftType::Narrowbody, 40},
+        {Airline::AircraftType::Widebody, 60},
+};
+
+const std::unordered_map<Airline::AircraftType, std::vector<char>> aircraftTypeSeatLetters = {
+    {Airline::AircraftType::Regional, {'A', 'B', 'C', 'D'}},
+    {Airline::AircraftType::Narrowbody, {'A', 'B', 'C', 'D', 'E', 'F'}},
+    {Airline::AircraftType::Widebody, {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K'}},
+};
+}
+
+// upon picking an aircraft type we can just generate one randomly by aircraft type, vector[aircraft_type]
