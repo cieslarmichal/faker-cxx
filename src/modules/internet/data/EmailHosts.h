2b93e9541fc1590e3b618c1a17f7493d6418dfaf
Michał Cieślar <michalovskyyy@gmail.com>
2023-11-07 01:00:15 +0100

    add generating http headers (#218)
    * add generating http headers
    
    * remove first name with spaces

--- src/modules/internet/data/EmailHosts.h
+++ src/modules/internet/data/EmailHosts.h
@@ -5,12 +5,7 @@
 
 namespace faker
 {
-const std::vector<std::string> emailHosts = {
-    "gmail.com",
-    "yahoo.com",
-    "hotmail.com",
-    "outlook.com",
-};
+const std::vector<std::string> emailHosts = {"gmail.com", "yahoo.com", "hotmail.com", "outlook.com"};
 
 const std::vector<std::string> emailExampleHosts = {"example.org", "example.com", "example.net"};
 
