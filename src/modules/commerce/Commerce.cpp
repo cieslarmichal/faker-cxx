f340697245d1c41b5e3b6c5341406fc4881683bb
Sebastián Bačík <sebastianlipkos@gmail.com>
2023-11-07 22:15:09 +0100

    methods fixed + tests written

--- src/modules/commerce/Commerce.cpp
+++ src/modules/commerce/Commerce.cpp
@@ -43,64 +43,99 @@ std::string Commerce::productFullName()
     return fmt::format("{} {} {}", productAdjective(), productMaterial(), productName());
 }
 
-std::string EAN13()
+std::string Commerce::EAN13()
 {
     std::string ean13 = String::numeric(12, false);
 
     int sum = 0;
     for (size_t i = 0; i < 12; i++)
     {
-        sum += (i % 2 == 0) ? (ean13[i] - '0') : 3 * (ean13[i] - '0');
+        if (i % 2 == 0) {
+            sum += (ean13[i] - '0');
+        }
+        else {
+            sum += 3 * (ean13[i] - '0');
+        }
     }
     
-    int checkDigit = 10 - (sum % 10);
+    int checkDigit = sum % 10;
+
+    if (checkDigit != 0) {
+        checkDigit = 10 - checkDigit;
+    }
 
     return ean13 + std::to_string(checkDigit);
 }
 
-std::string EAN8()
+std::string Commerce::EAN8()
 {
     std::string ean8 = String::numeric(7, false);
 
     int sum = 0;
     for (size_t i = 0; i < 7; i++)
     {
-        sum += (i % 2 == 0) ? (ean8[i] - '0') : 3 * (ean8[i] - '0');
+        if (i % 2 == 0) {
+            sum += 3 * (ean8[i] - '0');
+        }
+        else {
+            sum += (ean8[i] - '0');
+        }
     }
     
-    int checkDigit = 10 - (sum % 10);
+    int checkDigit = sum % 10;
+
+    if (checkDigit != 0) {
+        checkDigit = 10 - checkDigit;
+    }
 
     return ean8 + std::to_string(checkDigit);
 }
 
-std::string ISBN13()
+std::string Commerce::ISBN13()
 {
     std::string isbn13 = String::numeric(12, true);
 
     int sum = 0;
     for (size_t i = 0; i < 12; i++)
     {
-        sum += (i % 2 == 0) ? (isbn13[i] - '0') : 3 * (isbn13[i] - '0');
+        if (i % 2 == 0) {
+            sum += (isbn13[i] - '0');
+        }
+        else {
+            sum += 3 * (isbn13[i] - '0');
+        }
     }
     
-    int checkDigit = 10 - (sum % 10);
+    int checkDigit = sum % 10;
+
+    if (checkDigit != 0) {
+        checkDigit = 10 - checkDigit;
+    }
 
     return isbn13 + std::to_string(checkDigit);
 }
 
-std::string ISBN10()
+std::string Commerce::ISBN10()
 {
     std::string isbn10 = String::numeric(9, true);
 
     int sum = 0, weight = 10;
     for (size_t i = 0; i < 9; i++)
     {
-        sum += isbn10[i] * weight;
+        sum += (isbn10[i] - '0') * weight;
         weight--;
     }
     
-    int checkDigit = 10 - (sum % 10);
+    int checkDigit = sum % 11;
 
+    if (checkDigit != 0) {
+        checkDigit = 11 - checkDigit;
+    }
+
+    if (checkDigit == 10) {
+        return isbn10 + "X";
+    }
+    
     return isbn10 + std::to_string(checkDigit);
 }
 }
