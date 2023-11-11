6941a836e8b21073517a4dd0bbcc52cf8319022f
yakupcemilk <kayabasyakupcemil@gmail.com>
2023-11-12 01:49:50 +0300

    Food module is added.

---
+++ src/modules/food/data/seeds.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace faker {
+    const std::vector<std::string> seeds = {
+    "Chia Seeds",
+    "Flaxseeds",
+    "Sunflower Seeds",
+    "Pumpkin Seeds",
+    "Sesame Seeds",
+    "Poppy Seeds",
+    "Hemp Seeds",
+    "Quinoa",
+    "Pomegranate Seeds",
+    "Pine Nuts",
+    "Hemp Hearts",
+    "Sacha Inchi Seeds",
+    "Watermelon Seeds",
+    "Sunflower Kernels",
+    "Safflower Seeds",
+    "Alfalfa Seeds",
+    "Mustard Seeds",
+    "Coriander Seeds",
+    "Cumin Seeds",
+    "Fennel Seeds",
+    "Cardamom Seeds",
+    "Caraway Seeds",
+    "Ajwain Seeds",
+    "Nigella Seeds",
+    "Fenugreek Seeds",
+    "Anise Seeds",
+    "Celery Seeds",
+    "Dill Seeds",
+    "Cotton Seeds",
+    };
+}
