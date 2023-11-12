807b9dc526f7056235725b7e39074903b26cc65a
cieslarmichal <cieslarmichal@users.noreply.github.com>
2023-11-12 23:05:58 +0000

    deploy: e3742fad5998ecba2c0fe4b9db22a15fcda2ac06

--- search/all_14.js
+++ search/all_14.js
@@ -5,7 +5,8 @@ var searchData=
   ['weekdayabbreviatedname_2',['weekdayAbbreviatedName',['../classfaker_1_1Date.html#a27acfb21331807d8cfc0b3c9cfe57927',1,'faker::Date']]],
   ['weekdayname_3',['weekdayName',['../classfaker_1_1Date.html#a04494097d938c26becc981c1130af899',1,'faker::Date']]],
   ['weightedelement_4',['WeightedElement',['../structfaker_1_1Helper_1_1WeightedElement.html',1,'faker::Helper']]],
-  ['windspeed_5',['windspeed',['../structfaker_1_1Weather_1_1WindSpeed.html',1,'faker::Weather::WindSpeed'],['../classfaker_1_1Weather.html#a77001b0192ebcbca0c06d238462bfe94',1,'faker::Weather::windSpeed()']]],
-  ['word_6',['word',['../classfaker_1_1Word.html',1,'faker::Word'],['../classfaker_1_1Lorem.html#a8a9728179b08efab84505fbb7ea46eed',1,'faker::Lorem::word()']]],
-  ['words_7',['words',['../classfaker_1_1Lorem.html#aca2ee45cd6137dc43ea791497be506bd',1,'faker::Lorem::words()'],['../classfaker_1_1Word.html#ad2feb12057a72ae5cba55895da1f3d11',1,'faker::Word::words()']]]
+  ['westernzodiac_5',['westernZodiac',['../classfaker_1_1Person.html#ab3c42fd36e28a42ab8c0c73181d1135a',1,'faker::Person']]],
+  ['windspeed_6',['windspeed',['../structfaker_1_1Weather_1_1WindSpeed.html',1,'faker::Weather::WindSpeed'],['../classfaker_1_1Weather.html#a77001b0192ebcbca0c06d238462bfe94',1,'faker::Weather::windSpeed()']]],
+  ['word_7',['word',['../classfaker_1_1Word.html',1,'faker::Word'],['../classfaker_1_1Lorem.html#a8a9728179b08efab84505fbb7ea46eed',1,'faker::Lorem::word()']]],
+  ['words_8',['words',['../classfaker_1_1Lorem.html#aca2ee45cd6137dc43ea791497be506bd',1,'faker::Lorem::words()'],['../classfaker_1_1Word.html#ad2feb12057a72ae5cba55895da1f3d11',1,'faker::Word::words()']]]
 ];
