fc793d754d23c4806e8e13c22f0b44aaaf347807
MikelKokoshi <kokoshi67@gmail.com>
2023-11-24 20:10:47 +0100

    Add bioPart std::vector

---
+++ src/modules/person/data/Bio.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace faker{
+const std::vector<std::string> bioPart =
+{
+  "activist",
+  "artist",
+  "author",
+  "blogger",
+  "business owner",
+  "coach",
+  "creator",
+  "designer",
+  "developer",
+  "dreamer",
+  "educator",
+  "engineer",
+  "entrepreneur",
+  "environmentalist",
+  "film lover",
+  "filmmaker",
+  "foodie",
+  "founder",
+  "friend",
+  "gamer",
+  "geek",
+  "grad",
+  "inventor",
+  "leader",
+  "model",
+  "musician",
+  "nerd",
+  "parent",
+  "patriot",
+  "person",
+  "philosopher",
+  "photographer",
+  "public speaker",
+  "scientist",
+  "singer",
+  "streamer",
+  "student",
+  "teacher",
+  "traveler",
+  "veteran",
+  "writer",
+};
+}
