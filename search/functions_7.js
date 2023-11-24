af777732c12fd5c60c42edbf7b8faa4ed10d6cd6
cieslarmichal <cieslarmichal@users.noreply.github.com>
2023-11-24 09:26:24 +0000

    deploy: d11f90fc152eb93a7f046fa60cfe92045ac9b000

--- search/functions_7.js
+++ search/functions_7.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['hex_0',['hex',['../classfaker_1_1Color.html#a46e19893994e07e6d3dbdb37cd35cd63',1,'faker::Color::hex()'],['../classfaker_1_1Number.html#af958bfc1ada6c2470edfb13e93e24e11',1,'faker::Number::hex()']]],
-  ['hexadecimal_1',['hexadecimal',['../classfaker_1_1String.html#ae95d6815ba2172cc63374c391d246006',1,'faker::String']]],
+  ['hexadecimal_1',['hexadecimal',['../classfaker_1_1String.html#ae95d6815ba2172cc63374c391d246006',1,'faker::String::hexadecimal(unsigned length=1, HexCasing casing=HexCasing::Lower, HexPrefix prefix=HexPrefix::ZeroX)'],['../classfaker_1_1String.html#a9fbbf30a77e61856af68584305a41ca6',1,'faker::String::hexadecimal(GuaranteeMap &amp;&amp;guarantee, unsigned length=1, HexCasing casing=HexCasing::Lower, HexPrefix prefix=HexPrefix::ZeroX)']]],
   ['hobby_2',['hobby',['../classfaker_1_1Person.html#a499b411db884a8b154b370009bd3b3c2',1,'faker::Person']]],
   ['horse_3',['horse',['../classfaker_1_1Animal.html#a3f86d0fc7c4d2544935603a2e8753b81',1,'faker::Animal']]],
   ['hsl_4',['hsl',['../classfaker_1_1Color.html#ac76590c23d158333133277b6f6f2163a',1,'faker::Color']]],
