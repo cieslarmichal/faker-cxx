d89d1157368c1c7da826c57342b5b6411bd04f47
Mohit Singh <qmohitsingh@gmail.com>
2023-11-24 02:36:16 -0500

    feat::Add multilanguage sexes (Issue #251) 101a

--- include/faker-cxx/types/Sex.h
+++ include/faker-cxx/types/Sex.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Language.h"
+
 namespace faker
 {
 enum class Sex
@@ -26,6 +28,23 @@ const std::map<Language, std::map<Sex, std::string>> sexTranslations = {
     {Language::Ukrainian, {{Sex::Male, "Чоловік"}, {Sex::Female, "Жінка"}}},
     {Language::Danish, {{Sex::Male, "Mand"}, {Sex::Female, "Kvinde"}}},
     {Language::Swedish, {{Sex::Male, "Man"}, {Sex::Female, "Kvinna"}}},
+    {Language::Portuguese, {{Sex::Male, "Homem"}, {Sex::Female, "Mulher"}}},
+    {Language::Norwegian, {{Sex::Male, "Mann"}, {Sex::Female, "Kvinne"}}},
+    {Language::Japanese, {{Sex::Male, "男性"}, {Sex::Female, "女性"}}},
+    {Language::Hungarian, {{Sex::Male, "Férfi"}, {Sex::Female, "Nő"}}},
+    {Language::Croatian, {{Sex::Male, "Muškarac"}, {Sex::Female, "Žena"}}},
+    {Language::Greek, {{Sex::Male, "Άνδρας"}, {Sex::Female, "Γυναίκα"}}},
+    {Language::Slovene, {{Sex::Male, "Moški"}, {Sex::Female, "Ženska"}}},
+    {Language::Dutch, {{Sex::Male, "Man"}, {Sex::Female, "Vrouw"}}},
+    {Language::Mandarin, {{Sex::Male, "男"}, {Sex::Female, "女"}}},
+    {Language::Korean, {{Sex::Male, "남자"}, {Sex::Female, "여자"}}},
+    {Language::Serbian, {{Sex::Male, "Мушкарац"}, {Sex::Female, "Жена"}}},
+    {Language::Macedonian, {{Sex::Male, "Маж"}, {Sex::Female, "Жена"}}},
+    {Language::Albanian, {{Sex::Male, "Mashkull"}, {Sex::Female, "Femër"}}},
+    {Language::Latvian, {{Sex::Male, "Vīrietis"}, {Sex::Female, "Sieviete"}}},
+    {Language::Belarusian, {{Sex::Male, "Мужчына"}, {Sex::Female, "Жанчына"}}},
+    {Language::Estonian, {{Sex::Male, "Mees"}, {Sex::Female, "Naine"}}},
+    {Language::Irish, {{Sex::Male, "fireannach"}, {Sex::Female, "baineann"}}}
 };
 
 inline std::string translateSex(Sex sex, Language language = Language::English)
@@ -39,7 +58,7 @@ inline std::string translateSex(Sex sex, Language language = Language::English)
             return sexItr->second;
         }
     }
-    return "Unknown"; // Default or throw exception
+    return "Unknown";
 }
 
 inline std::string toString(Sex sex, Language language = Language::English)
