a47ded98b64e9912318ae2258661165803a7c762
Niharika Sai Duggi(Neethu) <60509922+Duggi-Niharika-Sai-06@users.noreply.github.com>
2023-11-29 14:10:35 -0500

    Update BulgarianLastNames.h

--- src/modules/person/data/bulgaria/BulgarianLastNames.h
+++ src/modules/person/data/bulgaria/BulgarianLastNames.h
@@ -1,1 +1,13 @@
+#pragma once
 
+#include <string>
+#include <vector>
+
+namespace faker
+{
+const std::vector<std::string> bulgarianLastNames = {
+    "Angelov", "Atanasov", "Bozhilov", "Dimitrov", "Draganov", "Georgiev", "Hristov", 
+    "Iliev", "Ivanov", "Kolev", "Kovachev", "Lazarov", "Mihaylov", "Nikolov", "Petrov",
+    "Popov", "Radev", "Simeonov", "Stefanov", "Stoyanov", "Todorov", "Vasilev", "Vladimirov",
+    "Yankov", "Zahariev"};
+}
