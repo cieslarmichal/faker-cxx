363f0344477a2d1ed35963cbb8ff7c0d37174b2a
Niharika Sai Duggi(Neethu) <60509922+Duggi-Niharika-Sai-06@users.noreply.github.com>
2023-11-29 14:16:50 -0500

    Update BulgarianPeopleNames.h

--- src/modules/person/data/bulgaria/BulgarianPeopleNames.h
+++ src/modules/person/data/bulgaria/BulgarianPeopleNames.h
@@ -1,1 +1,19 @@
+#pragma once
+
+#include "../NameFormats.h"
+#include "../PeopleNames.h"
+#include "BulgarianFirstNames.h"
+#include "BulgarianLastNames.h"
+
+namespace faker
+{
+const NameFormats bulgarianPersonNameFormats{
+    {{"{firstName} {lastName}", 10}}};
+
+const PeopleNames bulgarianPeopleNames{
+    {bulgarianMalesFirstNames, {}, bulgarianLastNames, {}, {}},
+    {bulgarianFemalesFirstNames, {}, bulgarianLastNames, {}, {}},
+    bulgarianPersonNameFormats};
+
+}
 
