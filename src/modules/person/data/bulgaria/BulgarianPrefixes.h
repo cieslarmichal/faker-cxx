d134c08d353ba6ae66f0091ee82f3a31f213bc93
Niharika Sai Duggi(Neethu) <60509922+Duggi-Niharika-Sai-06@users.noreply.github.com>
2023-11-29 14:23:40 -0500

    Create BulgarianPrefixes.h

---
+++ src/modules/person/data/bulgaria/BulgarianPrefixes.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace faker
+{
+const std::vector<std::string> bulgarianMalesPrefixes{"Г-н", "Д-р", "Проф."}; // "Г-н" is Mr., "Д-р" is Dr., "Проф." is Prof.
+
+const std::vector<std::string> bulgarianFemalesPrefixes{"Г-жа", "Г-ца", "Д-р", "Проф."}; // "Г-жа" is Mrs., "Г-ца" is Miss, "Д-р" is Dr., "Проф." is Prof.
+}
