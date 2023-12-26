0ba0d4bcbaf64d53e02bd4d3d9eca989f381217f
Ali Sedigh Moghadam <82403779+alisedighmoghadam@users.noreply.github.com>
2023-12-26 04:02:33 -0500

    Add Structure module (json, csv) (#386)
    * add structure module and JsonHelper
    
    * fix structure.cpp
    
    * fix typos and update moduleFunctions map
    
    * Remove stray submodule faker-cxx

---
+++ src/common/JsonHelper.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <sstream>
+
+namespace faker
+{
+
+class JsonHelper
+{
+public:
+// this function parse simple jason like {"key1":"value1","key2":"value2"}
+    static std::map<std::string, std::string> simpleJsonParser(std::string json);
+};
+}
