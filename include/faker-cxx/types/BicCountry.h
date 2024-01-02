d1000bb89d358d4607c5d23d3caf9efd0f93f791
Eric Floyd <101071809+eric-bodhi@users.noreply.github.com>
2024-01-02 03:03:53 -0500

    Expanded BIC Countries (#396)

--- include/faker-cxx/types/BicCountry.h
+++ include/faker-cxx/types/BicCountry.h
@@ -7,8 +7,20 @@ namespace faker
 enum class BicCountry
 {
     Poland,
+    United_States,
+    United_Kingdom,
+    Germany,
+    Romania,
+    France,
+    Italy,
+    Spain,
+    Netherlands,
+    India,
 };
 
-const std::vector<BicCountry> supportedBicCountries{BicCountry::Poland};
+const std::vector<BicCountry> supportedBicCountries{BicCountry::Poland, BicCountry::United_States,
+                                                    BicCountry::United_Kingdom, BicCountry::Germany,
+                                                    BicCountry::Romania, BicCountry::France, BicCountry::Italy,
+                                                    BicCountry::Spain, BicCountry::Netherlands, BicCountry::India};
 
 }
--- src/modules/finance/data/BankIndentifiersCodes.h
+++ src/modules/finance/data/BankIndentifiersCodes.h
@@ -11,7 +11,25 @@ namespace faker
 const std::map<BicCountry, std::vector<std::string>> bankIdentifiersCodesMapping = {
     {BicCountry::Poland,
      {"BPKOPLPW", "PKOPPLPW", "BREXPLPWMUL", "BNPAPLP", "POLUPLPR", "BIGBPLPW", "WBKPPLPP", "CITIPLPX", "INGBPLPW",
-      "DEUTPLPK", "DEUTPLP"}}
-
+      "DEUTPLPK", "DEUTPLP"}},
+    {BicCountry::United_States,
+     {"BOFAUS3N", "CITIUS33", "WELLSFARGO", "USBKUS44", "CHASUS33", "HSBCUS33", "PNCCUS33"}},
+    {BicCountry::United_Kingdom,
+     {"BARCGB22", "HSBCKENW", "LOYDGB21", "NWBKGB2L", "RBOSGB2L", "HSBCGB2L", "DEUTGB2L"}},
+    {BicCountry::Germany,
+     {"DEUTDEFF", "DRESDEFF", "COBADEFF", "BYLADEM1", "GENODEFF", "HYVEDEMM", "MALADE51", "NOLADE21", "SOLADEST",
+      "UNCRDEFF"}},
+    {BicCountry::Romania,
+     {"RNCBROBU", "BRDEROBU", "BTRLRO22", "PIRBROBU", "INGBROBU", "EXIMRO22", "CRDZROBU"}},
+    {BicCountry::France,
+     {"BNPAFRPP", "CEPAFRPP", "CRLYFRPP", "SOGEFRPP", "AGRIFRPP", "HSBDFRPP", "CCFRFRPP", "BNORDRPP", "CMCIFRPP"}},
+    {BicCountry::Italy,
+     {"UNCRITMM", "BCITITMM", "INTESA", "UBSPITPA", "BLOPIT22", "CITIITMX", "BNLIITRR"}},
+    {BicCountry::Spain,
+     {"CAIXESBB", "BBVAESMM", "SABSESBB", "BSCHESMM", "POPUESMM", "INGDESMM", "CITIES2X", "BCOEESMM"}},
+    {BicCountry::Netherlands,
+     {"ABNANL2A", "INGBNL2A", "RABONL2U", "TRIONL2U", "KNABNL2H", "SBINNL2X", "DEUTNL2N"}},
+    {BicCountry::India,
+     {"HDFCINBB", "ICICINBB", "SBININBB", "PNBAINBB", "UBININBB", "AXISINBB", "KKBKINBB", "YESBINBB", "IDBIINBB"}}
 };
 }
