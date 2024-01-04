997ba4cc1e37452da6dc141757cad6855e8a4f25
joshhn <duy.huynhngoc.05@gmail.com>
2024-01-03 22:19:27 -0600

    Add people name from Palestine

--- src/modules/person/PersonTest.cpp
+++ src/modules/person/PersonTest.cpp
@@ -20,6 +20,7 @@
 #include "data/england/EnglishPeopleNames.h"
 #include "data/estonia/EstonianPeopleNames.h"
 #include "data/finland/FinnishPeopleNames.h"
+#include "data/palestine/PalestinianPeopleNames.h"
 #include "data/france/FrenchPeopleNames.h"
 #include "data/Gender.h"
 #include "data/germany/GermanPeopleNames.h"
@@ -93,6 +94,7 @@ const std::map<Country, PeopleNames> countryToPeopleNamesMapping{
     {Country::Albania, albanianPeopleNames},      {Country::Iran, persianPeopleNames},
     {Country::Bulgaria, bulgarianPeopleNames},    {Country::Moldova, moldovanPeopleNames},
     {Country::Lithuania, lithuanianPeopleNames},  {Country::Iceland, icelandicPeopleNames},
+    {Country::Palestine, palestinianPeopleNames},
 };
 
 const std::map<Country, std::string> generatedTestName{
@@ -120,6 +122,7 @@ const std::map<Country, std::string> generatedTestName{
     {Country::Albania, "shouldGenerateAlbanianName"},      {Country::Iran, "shouldGeneratePersianName"},
     {Country::Bulgaria, "shouldGenerateBulgarianName"},    {Country::Moldova, "shouldGenerateMoldovanName"},
     {Country::Lithuania, "shouldGenerateLithuanianName"},  {Country::Iceland, "shouldGenerateIcelandicName"},
+    {Country::Palestine, "shouldGeneratePalestinianName"},
 };
 }
 
