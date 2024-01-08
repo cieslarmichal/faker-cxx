30a33f9e376e8466c5d0816ec6dc022ff014b6b6
ahronSteinbruch <asteinbruch96@gmail.com>
2024-01-08 12:41:49 +0200

    added the folder Bosnia with all necessary name files inside

---
+++ src/modules/person/data/Bosnia/BosnianLastNames.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace faker
+{
+const std::vector<std::string> bosnianLastNames = {
+    "Hod?i?",     "Mujagi?",        "Had?i?",      "Ahmetovi?",   "Ali?",        "Deli?",         "Osmanagi?",
+    "Hrnji?i?",   "Omeragi?",       "Sulji?",      "Kova?evi?",   "Brki?",       "Kurti?",        "Mehi?",
+    "Zuki?",      "Hasi?",          "Zimi?",       "Salihovi?",   "Dedi?",       "Koli?",         "D?afi?",
+    "Salki?",     "Omerhod?i?",     "Alagi?",      "Sijer?i?",    "Suljagi?",    "Dautovi?",      "Habibovi?",
+    "?au?evi?",   "Had?iahmetovi?", "Muminovi?",   "Brkan",       "?i?i?",       "Zvizdi?",       "Maksumi?",
+    "Pilav",      "?ehi?",          "Skopljak",    "Dizdarevi?",  "?engi?",      "Karahasanovi?", "Piri?",
+    "?i?i?",      "Muji?",          "Kujund?i?",   "Puzi?",       "Sulji?",      "Joldi?",        "Maksumi?",
+    "Hota",       "Kova?",          "Selimovi?",   "Terzi?",      "Malko?",      "Aganovi?",      "Poli?",
+    "?ati?",      "Dedi?",          "Okanovi?",    "Suljanovi?",  "Bajramovi?",  "Me?anovi?",     "Karahod?i?",
+    "?ampara",    "Nuki?",          "Rami?",       "Ibri?imovi?", "Musi?",       "Tali?",         "Zimi?",
+    "Zulfi?",     "Dizdar",         "Spahi?",      "Zaimovi?",    "Ibrahimovi?", "Mehi?",         "Kapi?",
+    "Be?lija",    "Zuki?",          "?ehi?",       "Mahmi?",      "Hod?i?",      "Osmi?",         "Zukan",
+    "Dedi?",      "Maksumi?",       "Tiro",        "Fazli?",      "Smaji?",      "Hati?",         "Muhamedagi?",
+    "Dervi?evi?", "Mujagi?",        "Mujezinovi?", "Husagi?",     "Zeki?",       "Mehanovi?",     "Malko?",
+    "Alagi?",     "Koli?",          "Nadarevi?",   "Sulji?",      "Omanovi?",    "Omanovi?",      "Huski?",
+    "Alji?",      "?i?i?",          "Begi?",       "Sijer?i?",    "?engi?",      "Kara?i?",       "Be?irba?i?",
+    "Smajlovi?",  "Mahmutovi?",     "Kapetanovi?", "Osmi?",       "Ibrahimagi?", "Had?ibegi?",    "Be?irovi?",
+    "Jusi?",      "Brki?",          "?ozi?",       "Musi?",       "?ehi?",       "Selimovi?",     "?ehi?",
+    "Muminovi?",  "Had?i?",         "Poli?",       "Hod?i?"};
+}
