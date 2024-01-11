d80c295a23499173a94644b7e7ccf9bd98987529
Nithiesh Mohan <nithiesh164@gmail.com>
2024-01-11 16:14:04 +0530

    Added Indian Location Support

---
+++ src/modules/location/data/india/IndiaStreetSuffixes.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace faker
+{
+const std::vector<std::string> indiaStreetSuffixes = {
+    "Pur",
+    "Puri",
+    "Halli",
+    "Wadi",
+    "Nagar",
+    "Abad",
+    "Palle",
+    "Pura",
+    "Patti",
+    "Uru",
+    "Oor",
+    "Gaon",
+    "Puram",
+    "Ong",
+    "Street",
+   };
+}
