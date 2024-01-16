b3ec70239990d0e9305374e2ae7070ee2cea8b83
jomorodi <omorojeff@gmail.com>
2024-01-16 17:20:59 +0200

    created estonia folder in location with neccesaay files

---
+++ src/modules/location/data/estonia/EstoniaStates.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace faker
+{
+const std::vector<std::string> finlandStates  {
+
+    "Harju",
+    "Hiiu",
+    "Ida-Viru",
+    "Jõgeva",
+    "Järva",
+    "Lääne",
+    "Lääne-Viru",
+    "Põlva",
+    "Pärnu",
+    "Rapla",
+    "Saare",
+    "Tartu",
+    "Valga",
+    "Viljandi",
+    "Võru",
+
+};
+
+}
