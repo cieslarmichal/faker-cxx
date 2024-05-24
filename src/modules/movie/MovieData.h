d44411a6add0326f0b6a7d41c597ea67e4b595b3
Uilian Ries <uilianries@gmail.com>
2024-05-24 21:07:32 +0200

    Add more movie data
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- src/modules/movie/MovieData.h
+++ src/modules/movie/MovieData.h
@@ -9,8 +9,8 @@ namespace faker
 {
 extern const std::array<std::string_view, 299> actors;
 extern const std::array<std::string_view, 300> actresses;
-extern const std::array<std::string_view, ?> directors;
-extern const std::array<std::string_view, ?> genres;
-extern const std::array<std::string_view, ?> movies;
-extern const std::array<std::string_view, ?> tvShows;
+extern const std::array<std::string_view, 300> directors;
+extern const std::array<std::string_view, 9> genres;
+extern const std::array<std::string_view, 250> movies;
+extern const std::array<std::string_view, 122> tvShows;
 }
