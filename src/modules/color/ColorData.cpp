b710137de6bdb868c25c671a2c22ce673f429560
Michał Cieślar <michalovskyyy@gmail.com>
2024-05-26 20:44:56 +0200

    refactor color module data (#593)

--- src/modules/color/ColorData.cpp
+++ src/modules/color/ColorData.cpp
@@ -1,11 +1,8 @@
-#pragma once
-
-#include <string>
-#include <vector>
+#include "ColorData.h"
 
 namespace faker
 {
-const std::vector<std::string> colors = {
+const std::array<std::string_view, 31> colors = {
     "red",      "green",  "blue",    "yellow", "purple", "mint green", "teal",      "white",
     "black",    "orange", "pink",    "grey",   "maroon", "violet",     "turquoise", "tan",
     "sky blue", "salmon", "plum",    "orchid", "olive",  "magenta",    "lime",      "ivory",
---
+++ src/modules/color/ColorData.h
@@ -0,0 +1,7 @@
+#include <array>
+#include <string_view>
+
+namespace faker
+{
+extern const std::array<std::string_view, 31> colors;
+}
