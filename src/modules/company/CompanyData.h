9b43eb5c71525aac721b59dc3a0c7411bc1fdf6c
Michał Cieślar <michalovskyyy@gmail.com>
2024-05-26 21:51:01 +0200

    refactor company module data (#595)

---
+++ src/modules/company/CompanyData.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <array>
+#include <string_view>
+
+namespace faker
+{
+extern const std::array<std::string_view, 65> buzzAdjectives;
+extern const std::array<std::string_view, 40> buzzNouns;
+extern const std::array<std::string_view, 60> buzzVerbs;
+extern const std::array<std::string_view, 100> catchPhraseAdjectives;
+extern const std::array<std::string_view, 101> catchPhraseDescriptors;
+extern const std::array<std::string_view, 104> catchPhraseNouns;
+extern const std::array<std::string_view, 8> companyTypes;
+extern const std::array<std::string_view, 147> companyIndustries;
+extern const std::array<std::string_view, 3> companySuffixes;
+}
