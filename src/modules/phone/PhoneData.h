7ad9f6e0a4f2ffc285ed578a6ce20ad3d0f6a324
Uilian Ries <uilianries@gmail.com>
2024-05-26 19:34:26 +0200

    fill phone data with vector files
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- src/modules/phone/PhoneData.h
+++ src/modules/phone/PhoneData.h
@@ -6,9 +6,9 @@
 
 namespace faker::phone
 {
-extern const std::array<std::string_view, 0> areaCodes;
-extern const std::array<std::string_view, 0> PhonePlatforms;
-extern const std::array<std::string_view, 0> PhoneModelNames;
-extern const std::array<std::string_view, 0> PhoneManufacturers;
-extern const std::array<std::string_view, 0> phoneNumbers;
+extern const std::array<std::string_view, 217> areaCodes;
+extern const std::array<std::string_view, 6> PhonePlatforms;
+extern const std::array<std::string_view, 15> PhoneModelNames;
+extern const std::array<std::string_view, 9> PhoneManufacturers;
+extern const std::array<std::string_view, 230> phoneNumbers;
 }
