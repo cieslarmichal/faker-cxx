0b8491389edc547ffa7d3b967e6cb898a9c317e6
cieslarmichal <cieslarmichal@users.noreply.github.com>
2024-05-27 16:52:43 +0000

    deploy: 6d235381211fa680495ef21d168409ce4a0b29db

--- search/all_6.js
+++ search/all_6.js
@@ -4,8 +4,8 @@ var searchData=
   ['gender_1',['gender',['../classfaker_1_1Person.html#ade55cc28e754c76d5763c01a9ba2f8b4',1,'faker::Person']]],
   ['genre_2',['genre',['../classfaker_1_1Book.html#a120db0e27afb1de546b3b126e8ca1cb6',1,'faker::Book::genre()'],['../classfaker_1_1Movie.html#a959dd0de296b7d7361925748549b4479',1,'faker::Movie::genre()'],['../classfaker_1_1Music.html#a998875e1a3c2b2a3ee9769f14da41488',1,'faker::Music::genre()'],['../classfaker_1_1VideoGame.html#adf3974c4d5000cfbbaa2217e307aa9b4',1,'faker::VideoGame::genre()']]],
   ['git_3',['Git',['../classfaker_1_1Git.html',1,'faker']]],
-  ['gpumanufacture_4',['gpuManufacture',['../classfaker_1_1Computer.html#a239041c447323eb589a46260f5d573ed',1,'faker::Computer']]],
-  ['gpumodel_5',['gpuModel',['../classfaker_1_1Computer.html#a3fe7bd82acc919179081064b4e6f1dc8',1,'faker::Computer']]],
-  ['gputype_6',['gpuType',['../classfaker_1_1Computer.html#a57eb88d08e1f5c11891509555edf5f60',1,'faker::Computer']]],
+  ['gpumanufacture_4',['gpuManufacture',['../classfaker_1_1Computer.html#ac658509cf31588798ee7a4e6fab5f80c',1,'faker::Computer']]],
+  ['gpumodel_5',['gpuModel',['../classfaker_1_1Computer.html#a4a4f8cd51b8e9590b34b86ff8bef87e5',1,'faker::Computer']]],
+  ['gputype_6',['gpuType',['../classfaker_1_1Computer.html#af03fcaa662b8f256ea6b49457b86e617',1,'faker::Computer']]],
   ['grain_7',['grain',['../classfaker_1_1Food.html#a09e59c0fad23f118aafde2b7d5211e42',1,'faker::Food']]]
 ];
