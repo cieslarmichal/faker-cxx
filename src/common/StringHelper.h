804d46a0ec00f470bf2420846a2698795862d983
Krupal J S Bhat <56032256+krupalbhat@users.noreply.github.com>
2024-06-08 12:38:41 +0530

    Modified stringHelper::Join to use string_view (#638)

--- src/common/StringHelper.h
+++ src/common/StringHelper.h
@@ -9,7 +9,8 @@ class StringHelper
 {
 public:
     static std::vector<std::string> split(const std::string& data, const std::string& separator = " ");
-    static std::string join(const std::vector<std::string>& data, const std::string& separator = " ");
+    static std::string joinString(const std::vector<std::string>& data, const std::string& separator = " ");
+    static std::string join(const std::vector<std::string_view>& data, const std::string& separator = " ");
     static std::string repeat(const std::string& data, int repetition);
     static std::string toLower(const std::string& data);
     static bool isPunctuation(char c);
