ff95aa80276ccaf2ef6fcac853df9852be33ed23
Sudarshan Jadhav <117652787+sjadhav07@users.noreply.github.com>
2024-06-10 15:19:29 +0530

    added plant module (#639)
    * added plant module
    
    * Added PlantTest to CMakeLists in test folder
    
    * renamed types to plantTypes

---
+++ include/faker-cxx/Plant.h
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <string_view>
+
+namespace faker
+{
+class Plant
+{
+public:
+    /**
+     * @brief Returns a random species of tree.
+     *
+     * @returns Species of tree.
+     *
+     * @code
+     * Plant::tree() // "Oak"
+     * @endcode
+     */
+    static std::string_view tree();
+
+    /**
+     * @brief Returns a random species of flower.
+     *
+     * @returns Species of flower.
+     *
+     * @code
+     * Plant::flower() // "Rose"
+     * @endcode
+     */
+    static std::string_view flower();
+
+    /**
+     * @brief Returns a random species of shrub.
+     *
+     * @returns Species of shrub.
+     *
+     * @code
+     * Plant::shrub() // "Azalea"
+     * @endcode
+     */
+    static std::string_view shrub();
+
+    /**
+     * @brief Returns a random species of grass.
+     *
+     * @returns Species of grass.
+     *
+     * @code
+     * Plant::grass() // "Kentucky Bluegrass"
+     * @endcode
+     */
+    static std::string_view grass();
+
+    /**
+     * @brief Returns a random species of fern.
+     *
+     * @returns Species of fern.
+     *
+     * @code
+     * Plant::fern() // "Maidenhair"
+     * @endcode
+     */
+    static std::string_view fern();
+
+    /**
+     * @brief Returns a random species of succulent.
+     *
+     * @returns Species of succulent.
+     *
+     * @code
+     * Plant::succulent() // "Aloe Vera"
+     * @endcode
+     */
+    static std::string_view succulent();
+
+    /**
+     * @brief Returns a random species of vine.
+     *
+     * @returns Species of vine.
+     *
+     * @code
+     * Plant::vine() // "Ivy"
+     * @endcode
+     */
+    static std::string_view vine();
+
+    /**
+     * @brief Returns a random type of plant.
+     *
+     * @returns Type of plant.
+     *
+     * @code
+     * Plant::type() // "tree"
+     * @endcode
+     */
+    static std::string_view plantType();
+};
+}
---
+++ src/modules/plant/PlantData.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <array>
+#include <string_view>
+
+namespace faker
+{
+extern const std::array<std::string_view, 47> trees;
+extern const std::array<std::string_view, 92> flowers;
+extern const std::array<std::string_view, 295> shrubs;
+extern const std::array<std::string_view, 97> grasses;
+extern const std::array<std::string_view, 87> ferns;
+extern const std::array<std::string_view, 132> succulents;
+extern const std::array<std::string_view, 24> vines;
+extern const std::array<std::string_view, 7> plantTypes;
+}
