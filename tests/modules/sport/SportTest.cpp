4fd336c527f7aaab3dc19c6a818ddf31e3bba016
Guru Mehar Rachaputi <gurumeharrachaputi@gmail.com>
2024-06-24 13:30:04 +0200

    refactor: sport module migrate (#684) (#717)
    * refactor: sport module migrate (#684)
    
    sport module migration from class to functions within sport namespace
    
    Signed-off-by: Guru Mehar Rachaputi <gurumeharrachaputi@gmail.com>
    
    * Update Sport.h
    
    Changed sport() function to sportName()
    
    * Update Sport.cpp
    
    Changed sport() to sportName()
    
    * Update SportData.h
    
    Changed "Sport" namespace to lowercase "sport"
    
    * Update SportTest.cpp
    
    Change sport() to sportName() and "Sport" to lowercase "sport" in sport namespace
    
    * refactor: sport module migrate (#684)
    
    	1. sport module migration from class to functions within sport namespace
    
    	2. changed function name from sport() to sportName()
    
    	3. changelog updated
    
    Signed-off-by: Guru Mehar Rachaputi <gurumeharrachaputi@gmail.com>
    
    ---------
    
    Signed-off-by: Guru Mehar Rachaputi <gurumeharrachaputi@gmail.com>

--- tests/modules/sport/SportTest.cpp
+++ tests/modules/sport/SportTest.cpp
@@ -9,6 +9,7 @@
 
 using namespace ::testing;
 using namespace faker;
+using namespace faker::sport;
 
 class SportTest : public Test
 {
@@ -17,41 +18,41 @@ public:
 
 TEST_F(SportTest, shouldGenerateSport)
 {
-    const auto generatedSport = Sport::sport();
+    const auto generatedSport = sportName();
 
-    ASSERT_TRUE(std::ranges::any_of(sport::sportNames, [generatedSport](const std::string_view& sport)
+    ASSERT_TRUE(std::ranges::any_of(sportNames, [generatedSport](const std::string_view& sport)
                                     { return sport == generatedSport; }));
 }
 
 TEST_F(SportTest, shouldGenerateSoccerTeam)
 {
-    const auto generatedSoccerTeam = Sport::soccerTeam();
+    const auto generatedSoccerTeam = soccerTeam();
 
-    ASSERT_TRUE(std::ranges::any_of(sport::soccerTeams, [generatedSoccerTeam](const std::string_view& soccerTeam)
+    ASSERT_TRUE(std::ranges::any_of(soccerTeams, [generatedSoccerTeam](const std::string_view& soccerTeam)
                                     { return soccerTeam == generatedSoccerTeam; }));
 }
 
 TEST_F(SportTest, shouldGenerateSportEvent)
 {
-    const auto generatedSportEvent = Sport::sportEvent();
+    const auto generatedSportEvent = sportEvent();
 
-    ASSERT_TRUE(std::ranges::any_of(sport::sportEvents, [generatedSportEvent](const std::string_view& sportEvent)
+    ASSERT_TRUE(std::ranges::any_of(sportEvents, [generatedSportEvent](const std::string_view& sportEvent)
                                     { return sportEvent == generatedSportEvent; }));
 }
 
 TEST_F(SportTest, shouldGenerateMaleAthlete)
 {
-    const auto generatedMaleAthlete = Sport::maleAthlete();
+    const auto generatedMaleAthlete = maleAthlete();
 
-    ASSERT_TRUE(std::ranges::any_of(sport::maleAthletes, [generatedMaleAthlete](const std::string_view& maleAthlete)
+    ASSERT_TRUE(std::ranges::any_of(maleAthletes, [generatedMaleAthlete](const std::string_view& maleAthlete)
                                     { return maleAthlete == generatedMaleAthlete; }));
 }
 
 TEST_F(SportTest, shouldGenerateFemaleAthlete)
 {
-    const auto generatedFemaleAthlete = Sport::femaleAthlete();
+    const auto generatedFemaleAthlete = femaleAthlete();
 
-    ASSERT_TRUE(std::ranges::any_of(sport::femaleAthletes,
+    ASSERT_TRUE(std::ranges::any_of(femaleAthletes,
                                     [generatedFemaleAthlete](const std::string_view& femaleAthlete)
                                     { return femaleAthlete == generatedFemaleAthlete; }));
 }
