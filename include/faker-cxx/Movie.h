1014b595511297990f48bff9732befabbb610401
Michal Cieslar <michalovskyyy@gmail.com>
2024-06-25 14:48:39 +0200

    refactor: change movie class to namespace

--- include/faker-cxx/Movie.h
+++ include/faker-cxx/Movie.h
@@ -2,75 +2,71 @@
 
 #include <string_view>
 
-namespace faker
+namespace faker::movie
 {
-class Movie
-{
-public:
-    /**
-     * @brief Returns a random movie genre.
-     *
-     * @returns Movie genre.
-     *
-     * @code
-     * Movie::genre() // "Drama"
-     * @endcode
-     */
-    static std::string_view genre();
+/**
+ * @brief Returns a random movie genre.
+ *
+ * @returns Movie genre.
+ *
+ * @code
+ * movie::genre() // "Drama"
+ * @endcode
+ */
+std::string_view genre();
 
-    /**
-     * @brief Returns a random movie title.
-     *
-     * @returns Movie title.
-     *
-     * @code
-     * Movie::movieTitle() // "Pulp Fiction"
-     * @endcode
-     */
-    static std::string_view movieTitle();
+/**
+ * @brief Returns a random movie title.
+ *
+ * @returns Movie title.
+ *
+ * @code
+ * movie::movieTitle() // "Pulp Fiction"
+ * @endcode
+ */
+std::string_view movieTitle();
 
-    /**
-     * @brief Returns a random tv show.
-     *
-     * @returns Tv show.
-     *
-     * @code
-     * Movie::tvShow() // "The Sopranos"
-     * @endcode
-     */
-    static std::string_view tvShow();
+/**
+ * @brief Returns a random tv show.
+ *
+ * @returns Tv show.
+ *
+ * @code
+ * movie::tvShow() // "The Sopranos"
+ * @endcode
+ */
+std::string_view tvShow();
 
-    /**
-     * @brief Returns a random movie director name.
-     *
-     * @returns Movie director name.
-     *
-     * @code
-     * Movie::director() // "Quentin Tarantino"
-     * @endcode
-     */
-    static std::string_view director();
+/**
+ * @brief Returns a random movie director name.
+ *
+ * @returns Movie director name.
+ *
+ * @code
+ * movie::director() // "Quentin Tarantino"
+ * @endcode
+ */
+std::string_view director();
 
-    /**
-     * @brief Returns a random actor name.
-     *
-     * @returns Actor name.
-     *
-     * @code
-     * Movie::actor() // "John Travolta"
-     * @endcode
-     */
-    static std::string_view actor();
+/**
+ * @brief Returns a random actor name.
+ *
+ * @returns Actor name.
+ *
+ * @code
+ * movie::actor() // "John Travolta"
+ * @endcode
+ */
+std::string_view actor();
 
-    /**
-     * @brief Returns a random actress name.
-     *
-     * @returns Actress name.
-     *
-     * @code
-     * Movie::actress() // "Scarlett Johansson"
-     * @endcode
-     */
-    static std::string_view actress();
-};
+/**
+ * @brief Returns a random actress name.
+ *
+ * @returns Actress name.
+ *
+ * @code
+ * movie::actress() // "Scarlett Johansson"
+ * @endcode
+ */
+std::string_view actress();
 }
