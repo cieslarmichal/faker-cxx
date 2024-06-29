f91e06f34ec52947771f0176cd49cc6c1a8a4bf1
Uilian Ries <uilianries@gmail.com>
2024-06-29 18:41:49 +0200

    Update export.h
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- include/faker-cxx/Export.h
+++ include/faker-cxx/Export.h
@@ -1,46 +1,46 @@
 #ifndef FAKER_CXX_EXPORT_H
 #define FAKER_CXX_EXPORT_H
 
-#ifndef _MSVC
-#  define FAKER_CXX_EXPORT
-#  define FAKER_CXX_NO_EXPORT
-#endif
+#ifdef _MSVC
+#  ifdef FAKER_CXX_STATIC_DEFINE
+#    define FAKER_CXX_EXPORT
+#    define FAKER_CXX_NO_EXPORT
+#  else
+#    ifndef FAKER_CXX_EXPORT
+#      ifdef faker_cxx_EXPORTS
+         /* We are building this library */
+#        define FAKER_CXX_EXPORT __declspec(dllexport)
+#      else
+          /* We are using this library */
+#        define FAKER_CXX_EXPORT __declspec(dllimport)
+#      endif
+#    endif
 
-#ifdef FAKER_CXX_STATIC_DEFINE
-#  define FAKER_CXX_EXPORT
-#  define FAKER_CXX_NO_EXPORT
-#else
-#  ifndef FAKER_CXX_EXPORT
-#    ifdef faker_cxx_EXPORTS
-        /* We are building this library */
-#      define FAKER_CXX_EXPORT __declspec(dllexport)
-#    else
-        /* We are using this library */
-#      define FAKER_CXX_EXPORT __declspec(dllimport)
+#    ifndef FAKER_CXX_NO_EXPORT
+#      define FAKER_CXX_NO_EXPORT
 #    endif
 #  endif
 
-#  ifndef FAKER_CXX_NO_EXPORT
-#    define FAKER_CXX_NO_EXPORT
+#  ifndef FAKER_CXX_DEPRECATED
+#    define FAKER_CXX_DEPRECATED __declspec(deprecated)
 #  endif
-#endif
 
-#ifndef FAKER_CXX_DEPRECATED
-#  define FAKER_CXX_DEPRECATED __declspec(deprecated)
-#endif
-
-#ifndef FAKER_CXX_DEPRECATED_EXPORT
-#  define FAKER_CXX_DEPRECATED_EXPORT FAKER_CXX_EXPORT FAKER_CXX_DEPRECATED
-#endif
-
-#ifndef FAKER_CXX_DEPRECATED_NO_EXPORT
-#  define FAKER_CXX_DEPRECATED_NO_EXPORT FAKER_CXX_NO_EXPORT FAKER_CXX_DEPRECATED
-#endif
-
-#if 0 /* DEFINE_NO_DEPRECATED */
-#  ifndef FAKER_CXX_NO_DEPRECATED
-#    define FAKER_CXX_NO_DEPRECATED
+#  ifndef FAKER_CXX_DEPRECATED_EXPORT
+#    define FAKER_CXX_DEPRECATED_EXPORT FAKER_CXX_EXPORT FAKER_CXX_DEPRECATED
 #  endif
+
+#  ifndef FAKER_CXX_DEPRECATED_NO_EXPORT
+#    define FAKER_CXX_DEPRECATED_NO_EXPORT FAKER_CXX_NO_EXPORT FAKER_CXX_DEPRECATED
+#  endif
+
+#  if 0 /* DEFINE_NO_DEPRECATED */
+#    ifndef FAKER_CXX_NO_DEPRECATED
+#      define FAKER_CXX_NO_DEPRECATED
+#    endif
+#  endif
+#else
+#  define FAKER_CXX_EXPORT
+#  define FAKER_CXX_NO_EXPORT
 #endif
 
 #endif /* FAKER_CXX_EXPORT_H */
