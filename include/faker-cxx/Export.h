48a2ba3fe7dd307768a3cd765d3b25d906a828a4
Uilian Ries <uilianries@gmail.com>
2024-06-29 18:06:29 +0200

    Add export file
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ include/faker-cxx/Export.h
@@ -0,0 +1,41 @@
+#ifndef FAKER_CXX_EXPORT_H
+#define FAKER_CXX_EXPORT_H
+
+#ifdef FAKER_CXX_STATIC_DEFINE
+#  define FAKER_CXX_EXPORT
+#  define FAKER_CXX_NO_EXPORT
+#else
+#  ifndef FAKER_CXX_EXPORT
+#    ifdef faker_cxx_EXPORTS
+        /* We are building this library */
+#      define FAKER_CXX_EXPORT __declspec(dllexport)
+#    else
+        /* We are using this library */
+#      define FAKER_CXX_EXPORT __declspec(dllimport)
+#    endif
+#  endif
+
+#  ifndef FAKER_CXX_NO_EXPORT
+#    define FAKER_CXX_NO_EXPORT
+#  endif
+#endif
+
+#ifndef FAKER_CXX_DEPRECATED
+#  define FAKER_CXX_DEPRECATED __declspec(deprecated)
+#endif
+
+#ifndef FAKER_CXX_DEPRECATED_EXPORT
+#  define FAKER_CXX_DEPRECATED_EXPORT FAKER_CXX_EXPORT FAKER_CXX_DEPRECATED
+#endif
+
+#ifndef FAKER_CXX_DEPRECATED_NO_EXPORT
+#  define FAKER_CXX_DEPRECATED_NO_EXPORT FAKER_CXX_NO_EXPORT FAKER_CXX_DEPRECATED
+#endif
+
+#if 0 /* DEFINE_NO_DEPRECATED */
+#  ifndef FAKER_CXX_NO_DEPRECATED
+#    define FAKER_CXX_NO_DEPRECATED
+#  endif
+#endif
+
+#endif /* FAKER_CXX_EXPORT_H */
