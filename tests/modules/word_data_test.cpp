549fc7edb659e1c30d8873e4b7c94ce995176c32
Jack Walters <jackwalterswork@gmail.com>
2024-07-28 15:44:53 +0100

    Specify namespace and include algorithm header

--- tests/modules/word_data_test.cpp
+++ tests/modules/word_data_test.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
-#include <vector>
+#include <algorithm>
 #include <iterator>
+#include <vector>
 
 #include "word_data.h"
 
@@ -70,7 +71,7 @@ TEST_F(WordDataTest, shouldSwapDifferentContainers)
 TEST_F(WordDataTest, shouldPartitionEvenOdd)
 {
     std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    auto it = partition(vec.begin(), vec.end(), [](int n) { return n % 2 == 0; });
+    auto it = faker::word::cstd::partition(vec.begin(), vec.end(), [](int n) { return n % 2 == 0; });
     ASSERT_TRUE(std::all_of(vec.begin(), it, [](int n) { return n % 2 == 0; }));
     ASSERT_TRUE(std::all_of(it, vec.end(), [](int n) { return n % 2 != 0; }));
 }
@@ -78,7 +79,7 @@ TEST_F(WordDataTest, shouldPartitionEvenOdd)
 TEST_F(WordDataTest, shouldPartitionGreaterThanFive)
 {
     std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    auto it = partition(vec.begin(), vec.end(), [](int n) { return n > 5; });
+    auto it = faker::word::cstd::partition(vec.begin(), vec.end(), [](int n) { return n > 5; });
     ASSERT_TRUE(std::all_of(vec.begin(), it, [](int n) { return n > 5; }));
     ASSERT_TRUE(std::all_of(it, vec.end(), [](int n) { return n <= 5; }));
 }
