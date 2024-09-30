1883bc7d615bc41c770a63e00af5540cd9def7e6
Guru Mehar Rachaputi <gurumeharrachaputi@gmail.com>
2024-09-30 16:25:08 +0200

    chore: added weather descriptions
    * added weather descriptions to finland, germany, poland, czechia and
      france
    
    Signed-off-by: Guru Mehar Rachaputi <gurumeharrachaputi@gmail.com>

--- src/modules/weather_data.h
+++ src/modules/weather_data.h
@@ -42,4 +42,90 @@ const auto svSEWeatherDescriptions = std::to_array<std::string_view>({
     "höst", // fall/autumn
 });
 
+const auto csCZWeatherDescriptions = std::to_array<std::string_view>({
+    "dusno",
+    "chladno",
+    "horko",
+    "jasno",
+    "mlhavo",
+    "oblačno",
+    "slunečno",
+    "sucho",
+    "teplo",
+    "vedro",
+    "vlhko",
+    "zamračeno",
+    "zima",
+    "příjemně",
+    "polojasno",
+});
+
+const auto frFRWeatherDescriptions = std::to_array<std::string_view>({
+    "enneigée",
+    "ensoleillée",
+    "humide",
+    "nuageux",
+    "orageux",
+    "sèche",
+    "venteuse",
+    "pleut",
+    "pleut à verse",
+    "du soleil",
+    "du vent",
+    "du brouillard",
+    "neige",
+    "gèle",
+    "grêle",
+    "mauvais",
+});
+
+const auto plPLWeatherDescriptions = std::to_array<std::string_view>({
+    "pogoda",
+    "słońce",
+    "deszcz",
+    "śnieg",
+    "wiatr",
+    "chmura",
+    "mgła",
+    "lód",
+    "burza",
+    "grzmot",
+    "piorun",
+    "śnieżyca",
+});
+
+const auto deDEWeatherDescriptions = std::to_array<std::string_view>({
+    "sonnig",
+    "wolkig",
+    "windig",
+    "regnerisch",
+    "verregnet",
+    "neblig",
+    "diesig",
+    "trüb",
+    "stürmisch",
+    "schnee",
+    "hagel",
+    "nebel",
+});
+
+const auto fiFIWeatherDescriptions = std::to_array<std::string_view>({
+    "sumu",
+    "sade",
+    "lumi",
+    "aurinkoinen",
+    "tuuli",
+    "jää",
+    "myrsky",
+    "pilvi",
+    "rankkasade",
+    "kostea",
+    "sadekuuro",
+    "kuura",
+    "rae",
+    "räntä",
+});
+
 }
+
+
