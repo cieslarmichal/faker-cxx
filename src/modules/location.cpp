3bc5c17c69e7a31e1386df3d7b6947c4d61e4af8
Filip Vateha <vatehafilip@gmail.com>
2024-10-03 19:27:34 +0200

    implemented feature nearbyGPSCoordinate()

--- src/modules/location.cpp
+++ src/modules/location.cpp
@@ -211,4 +211,46 @@ std::string_view timeZone()
     return helper::randomElement(timeZones);
 }
 
+std::tuple<std::string, std::string> nearbyGPSCoordinate(
+    Precision precision,
+    const std::tuple<double, double>& origin,
+    const double radius,
+    const bool isMetric)
+{
+    // If origin is not provided, generate a random GPS coordinate.
+    if (std::get<0>(origin) == std::numeric_limits<double>::max() &&
+        std::get<1>(origin) == std::numeric_limits<double>::max())
+    {
+        return { latitude(precision), longitude(precision) };
+    }
+    
+    const auto angleRadians = number::decimal<double>(2 * M_PI);
+
+    const auto radiusMetric = isMetric ? radius : radius * 1.60934;
+    const auto distanceInKm = number::decimal<double>(radiusMetric);
+
+    constexpr auto kmPerDegree = 40000 / 360; //The distance in km per degree for earth.
+    const auto distanceInDegree = distanceInKm / kmPerDegree;
+
+    auto coordinateLatitude = std::get<0>(origin) + distanceInDegree * std::sin(angleRadians);
+    auto coordinateLongitude = std::get<1>(origin) + distanceInDegree * std::cos(angleRadians);
+
+    // Box the latitude [-90, 90]
+    coordinateLatitude = std::fmod(coordinateLatitude, 180.0);
+    if (coordinateLatitude < -90.0 || coordinateLatitude > 90.0)
+    {
+        coordinateLatitude = std::copysign(180.0, coordinateLatitude) - coordinateLatitude;
+        coordinateLongitude += 180.0;
+    }
+
+    // Box the longitude [-180, 180]
+    coordinateLongitude = std::fmod(std::fmod(coordinateLongitude, 360.0) + 540.0, 360.0) - 180.0;
+
+    return
+    {
+        common::precisionFormat(precision, coordinateLatitude),
+        common::precisionFormat(precision, coordinateLongitude)
+    };
+}
+
 }
