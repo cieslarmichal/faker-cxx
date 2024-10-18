6a43b292b7e07a7f49b8aee8e41a9666f6c28d85
viliam.poproci <viliam.poproci@gmail.com>
2024-10-18 10:33:58 +0200

    add locale to esports module

--- include/faker-cxx/esport.h
+++ include/faker-cxx/esport.h
@@ -3,11 +3,14 @@
 #include <string_view>
 
 #include "faker-cxx/export.h"
+#include "faker-cxx/types/locale.h"
 
 namespace faker::esport
 {
 /**
  * @brief Returns a random player name.
+ *
+ * @param locale The locale. Defaults to `Locale::en_US`.
  *
  * @return Player name.
  *
@@ -15,10 +18,12 @@ namespace faker::esport
  * faker::esport::player() // "Faker"
  * @endcode
  */
-FAKER_CXX_EXPORT std::string_view player();
+FAKER_CXX_EXPORT std::string_view player(Locale locale = Locale::en_US);
 
 /**
  * @brief Returns a random esport team.
+ *
+ * @param locale The locale. Defaults to `Locale::en_US`.
  *
  * @return Esport team.
  *
@@ -26,10 +31,12 @@ FAKER_CXX_EXPORT std::string_view player();
  * faker::esport::team() // "Karmine Corp"
  * @endcode
  */
-FAKER_CXX_EXPORT std::string_view team();
+FAKER_CXX_EXPORT std::string_view team(Locale locale = Locale::en_US);
 
 /**
  * @brief Returns a random esport league.
+ *
+ * @param locale The locale. Defaults to `Locale::en_US`.
  *
  * @return Esport League.
  *
@@ -37,10 +44,12 @@ FAKER_CXX_EXPORT std::string_view team();
  * faker::esport::league() // "LEC"
  * @endcode
  */
-FAKER_CXX_EXPORT std::string_view league();
+FAKER_CXX_EXPORT std::string_view league(Locale locale = Locale::en_US);
 
 /**
  * @brief Returns a random esport event.
+ *
+ * @param locale The locale. Defaults to `Locale::en_US`.
  *
  * @return Esport Event.
  *
@@ -48,10 +57,12 @@ FAKER_CXX_EXPORT std::string_view league();
  * faker::esport::event() // "Faker"
  * @endcode
  */
-FAKER_CXX_EXPORT std::string_view event();
+FAKER_CXX_EXPORT std::string_view event(Locale locale = Locale::en_US);
 
 /**
  * @brief Returns a random competitive video game.
+ *
+ * @param locale The locale. Defaults to `Locale::en_US`.
  *
  * @return Competitive video game.
  *
@@ -59,5 +70,5 @@ FAKER_CXX_EXPORT std::string_view event();
  * faker::esport::game() // "Rocket League"
  * @endcode
  */
-FAKER_CXX_EXPORT std::string_view game();
+FAKER_CXX_EXPORT std::string_view game(Locale locale = Locale::en_US);
 }
