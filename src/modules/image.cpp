9b7d709354a80d5bae094b6210aab44360b955ff
bitalec <75757163+bitalec@users.noreply.github.com>
2024-11-25 21:08:11 +0100

    fix: fix imageUrl in Image module to generate images from picsum photos #990 (#998)
    * create method urlPicsumPhotos for generate random image from https://picsum.photos and changed the name of urlImage in urlLoremFlickr
    
    * created imageUrl: Generates a random image url with `https://loremflickr.com/` or "https://picsum.photos"
    
    * created imageUrl: Generates a random image url with `https://loremflickr.com/` or "https://picsum.photos"

--- src/modules/image.cpp
+++ src/modules/image.cpp
@@ -1,6 +1,7 @@
 #include "faker-cxx/image.h"
 
 #include <array>
+#include <faker-cxx/datatype.h>
 #include <optional>
 #include <string>
 #include <string_view>
@@ -25,12 +26,45 @@ std::unordered_map<ImageCategory, std::string> imageCategoryToLoremFlickrStringM
 };
 }
 
-std::string imageUrl(unsigned int width, unsigned int height, std::optional<ImageCategory> category)
+std::string imageUrl(unsigned width, unsigned height)
+{
+    if (faker::datatype::boolean(0.5))
+    {
+        return urlLoremFlickr();
+    }
+    else
+        return urlPicsumPhotos();
+}
+
+std::string urlLoremFlickr(unsigned int width, unsigned int height, std::optional<ImageCategory> category)
 {
     const std::string image_category =
         category.has_value() ? common::format("/{}", imageCategoryToLoremFlickrStringMapping.at(category.value())) : "";
 
     return common::format("https://loremflickr.com/{}/{}{}", width, height, image_category);
+}
+
+std::string urlPicsumPhotos(unsigned width, unsigned height, const std::optional<bool> greyscale,
+                            const std::optional<int> blur)
+{
+    std::string url = common::format("https://picsum.photos/{}/{}", width, height);
+    std::string params;
+
+    params += (greyscale.has_value() && greyscale.value()) ? "greyscale" : "";
+
+    if (blur.has_value())
+    {
+        if (blur.value() < 1 || blur.value() > 10)
+            return common::format("{}", "Error: blur must be between 1 and 10.");
+        if (!params.empty())
+            params += "&";
+        params += common::format("blur={}", blur.value());
+    }
+    if (!params.empty())
+    {
+        url = common::format("{}?{}", url, params);
+    }
+    return common::format("{}", url);
 }
 
 std::string githubAvatarUrl()
