33cec93754a014a7b1b9bba5a3c4fa9db39b4ab3
Vishnu Srivatsava <49309611+VishnuSrivatsava@users.noreply.github.com>
2025-01-15 18:24:57 +0530

    fixed namespace prefix

--- include/faker-cxx/base64.h
+++ include/faker-cxx/base64.h
@@ -3,7 +3,7 @@
 
 #include <string>
 
-namespace base64 {
+namespace faker::base64 {
     std::string encode(const std::string &input);
     std::string decode(const void* data, const size_t len);
 }
