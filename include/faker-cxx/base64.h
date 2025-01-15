09725c493322e97de6a6e7a3bba27e4e9042020c
Vishnu Srivatsava <49309611+VishnuSrivatsava@users.noreply.github.com>
2025-01-15 18:41:22 +0530

    Replace include guards with #pragma once in base64.h

--- include/faker-cxx/base64.h
+++ include/faker-cxx/base64.h
@@ -1,5 +1,4 @@
-#ifndef BASE64_H
-#define BASE64_H
+#pragma once
 
 #include <string>
 
@@ -7,5 +6,3 @@ namespace faker::base64 {
     std::string encode(const std::string &input);
     std::string decode(const void* data, const size_t len);
 }
-
-#endif
