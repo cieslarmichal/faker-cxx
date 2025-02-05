e9f38e0e0835a288c99bc8484b70be8a9715838f
Florian <florian@motherduck.com>
2025-02-05 21:02:05 +0100

    Export symbols in generator.h

--- include/faker-cxx/generator.h
+++ include/faker-cxx/generator.h
@@ -2,13 +2,15 @@
 
 #include <random>
 
+#include "faker-cxx/export.h"
+
 namespace faker
 {
 
-std::mt19937_64& getGenerator();
+FAKER_CXX_EXPORT std::mt19937_64& getGenerator();
 
-void setSeed(std::mt19937_64::result_type seed);
+FAKER_CXX_EXPORT void setSeed(std::mt19937_64::result_type seed);
 
-void setSeed(std::seed_seq& seq);
+FAKER_CXX_EXPORT void setSeed(std::seed_seq& seq);
 
 }
