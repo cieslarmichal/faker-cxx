9b33f39d5b8f1756ff537819a81ada1f411f5fd2
Michał Cieślar <michalovskyyy@gmail.com>
2026-08-16 18:12:36 +0200

    feat: add location address data for Argentina (es_AR)

--- tests/modules/location_test.cpp
+++ tests/modules/location_test.cpp
@@ -112,6 +112,8 @@ CountryAddressesInfo getAddresses(const Locale& locale)
         return lithuaniaAddresses;
     case Locale::sq_AL:
         return albaniaAddresses;
+    case Locale::es_AR:
+        return argentinaAddresses;
     default:
         return usaAddresses;
     }
