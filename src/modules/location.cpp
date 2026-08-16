33dadc835fd7572f3339c5e90bb49ef79a766827
Michał Cieślar <michalovskyyy@gmail.com>
2026-08-16 18:12:34 +0200

    feat: add location address data for Greece (el_GR)

--- src/modules/location.cpp
+++ src/modules/location.cpp
@@ -113,6 +113,8 @@ CountryAddressesInfo getAddresses(const Locale& locale)
         return lithuaniaAddresses;
     case Locale::sq_AL:
         return albaniaAddresses;
+    case Locale::el_GR:
+        return greeceAddresses;
     default:
         return usaAddresses;
     }
